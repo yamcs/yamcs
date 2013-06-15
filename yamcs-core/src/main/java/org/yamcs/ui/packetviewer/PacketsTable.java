58ba50d18115910fd07c8c7d046fa9d811e6295d
Fabian Diet <fabian.diet@spaceapplications.com>
2013-06-15 14:23:02 +0200

    Add Pop-up > Mark instead of star column
    Replaced the just-introduced star/unstar column with a
    less visible right-click -> mark/unmark action.
    
    This commit also brings the following changes:
     - Right-align known number columns in packets table
     - Show vertical grid lines

--- yamcs-core/src/main/java/org/yamcs/ui/packetviewer/PacketsTable.java
+++ yamcs-core/src/main/java/org/yamcs/ui/packetviewer/PacketsTable.java
@@ -1,22 +1,31 @@
 package org.yamcs.ui.packetviewer;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Comparator;
-import java.util.Vector;
+import java.util.HashSet;
+import java.util.Set;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableRowSorter;
 
 import org.yamcs.ui.PacketListener;
@@ -27,40 +36,60 @@ import org.yamcs.utils.TimeEncoding;
 public class PacketsTable extends JTable implements ListSelectionListener, PacketListener {
 
     private static final long serialVersionUID = 1L;
-    private static final String[] COLUMNS = {"", "Generation Time", "APID", "Opsname", "Size"};
-
-    private static final ImageIcon ICON_UNSTARRED = new ImageIcon(PacketsTable.class.getResource("/org/yamcs/images/unstarred.png"));
-    private static final ImageIcon ICON_STARRED = new ImageIcon(PacketsTable.class.getResource("/org/yamcs/images/starred.png"));
+    private static final String[] COLUMNS = {"Generation Time", "APID", "Opsname", "Size"};
+    private static final String MARK_PACKET = "Mark packet";
+    private static final String UNMARK_PACKET = "Unmark packet";
 
     private PacketViewer packetViewer;
+    private JPopupMenu popup; 
+    private JMenuItem markPacketMenuItem;
     private int maxLines = 1000;
+
+    private Set<Integer> markedRows = new HashSet<Integer>(2);
+
+    private Action markPacketAction;
+    private Action unmarkPacketAction;
 
     public PacketsTable(PacketViewer packetViewer) {
         super();
         this.packetViewer = packetViewer;
 
-        DefaultTableModel packetsModel = new DefaultTableModel(COLUMNS, 0);
+        DefaultTableModel packetsModel = new DefaultTableModel(COLUMNS, 0) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Class<?> getColumnClass(int columnIndex) { 
+                if (columnIndex == 1 || columnIndex == 3)
+                    return Number.class;
+                return super.getColumnClass(columnIndex);
+            }
+        };
         setModel(packetsModel);
 
         setPreferredScrollableViewportSize(getPreferredSize());
         setFillsViewportHeight(true);
-        getColumnModel().getColumn(0).setPreferredWidth(50);
-        getColumnModel().getColumn(1).setPreferredWidth(330);
-        getColumnModel().getColumn(2).setPreferredWidth(50);
-        getColumnModel().getColumn(3).setPreferredWidth(280);
+        getColumnModel().getColumn(0).setPreferredWidth(160);
+        getColumnModel().getColumn(1).setPreferredWidth(50);
+        getColumnModel().getColumn(2).setPreferredWidth(160);
+        getColumnModel().getColumn(3).setPreferredWidth(50);
 
         setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
-        setShowGrid(false);
+        setShowHorizontalLines(false);
+        setGridColor(new Color(216, 216, 216));
         setIntercellSpacing(new Dimension(0, 0));
+        setRowHeight(getRowHeight() + 2);
 
         TableRowSorter<DefaultTableModel> packetsSorter = new TableRowSorter<DefaultTableModel>(packetsModel);
-        packetsSorter.setComparator(2, new Comparator<Number>() {
+
+        Comparator<Number> numberComparator = new Comparator<Number>() {
             @Override
             public int compare(Number o1, Number o2) {
                 return o1.intValue() < o2.intValue() ? -1 : (o1.intValue() > o2.intValue() ? 1 : 0);
             }
-        });
+        };
+        packetsSorter.setComparator(1, numberComparator);
+        packetsSorter.setComparator(3, numberComparator);
         setRowSorter(packetsSorter);
 
         // Swing highlights the selected cell with an annoying blue border.
@@ -77,33 +106,45 @@ public class PacketsTable extends JTable implements ListSelectionListener, Packe
             }
         });
 
-        setDefaultRenderer(ImageIcon.class, new IconRenderer());
+        DefaultTableCellRenderer numberRenderer = new DefaultTableCellRenderer() {
+            private static final long serialVersionUID = 1L;
 
-        addMouseListener(new TableMouseListener());
+            @Override
+            public Component getTableCellRendererComponent(JTable table,
+                    Object value, boolean isSelected, boolean hasFocus,
+                    int row, int column) {
+                return super.getTableCellRendererComponent(table, value,
+                        isSelected, false /* disable focus ! */, row, column);
+            }
+        };
+        numberRenderer.setHorizontalAlignment(SwingConstants.RIGHT);
+        setDefaultRenderer(Number.class, numberRenderer);
+
+        createActions();
+        createPopupMenu();
+    }
+
+    @Override
+    public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+        Component component = super.prepareRenderer(renderer, row, column);
+        if (popup.isShowing() && isCellSelected(row, column)) {
+            component.setBackground(new Color(216, 216, 216));
+        } else if (!isCellSelected(row, column)) {
+            if (markedRows.contains(row))
+                component.setBackground(Color.YELLOW);
+            else
+                component.setBackground(Color.WHITE);
+        }
+        return component;
     }
 
     public void clear() {
         ((DefaultTableModel) getModel()).setRowCount(0);
+        markedRows.clear();
     }
 
     public void addRow(Object[] rowData) {
-        Vector<Object> v = new Vector<Object>(rowData.length + 1);
-        for (Object o : rowData) {
-            v.addElement(o);
-        }
-
-        v.add(0, ICON_UNSTARRED);
-        ((DefaultTableModel) getModel()).addRow(v);
-    }
-
-    @Override
-    public Class<?> getColumnClass(int column) {
-        int realColumn = convertColumnIndexToModel(column);
-        if (realColumn == 0) {
-            return ICON_UNSTARRED.getClass();
-        } else {
-            return super.getColumnClass(column);
-        }
+        ((DefaultTableModel) getModel()).addRow(rowData);
     }
 
     public void setMaxLines(int maxLines) {
@@ -115,13 +156,56 @@ public class PacketsTable extends JTable implements ListSelectionListener, Packe
         return false;
     }
 
+    private void createActions() {
+        markPacketAction = new AbstractAction(MARK_PACKET) {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int row = getSelectedRow();
+                if (row != -1) markedRows.add(row);
+            }
+        };
+
+        unmarkPacketAction = new AbstractAction(UNMARK_PACKET) {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int row = getSelectedRow();
+                if (row != -1) markedRows.remove(row);
+            }
+        };
+    }
+
+    private void createPopupMenu() {
+        popup = new JPopupMenu();
+        markPacketMenuItem = new JMenuItem(markPacketAction);
+        popup.add(markPacketMenuItem);
+        popup.addPopupMenuListener(new PopupMenuListener() {
+            @Override
+            public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
+                // Make sure selection background of entire row is updated
+                // Not just the parts that were covered by the popup.
+                repaint();
+            }
+            @Override public void popupMenuWillBecomeVisible(PopupMenuEvent e) {}
+            @Override public void popupMenuCanceled(PopupMenuEvent e) {}
+        });
+        MouseListener popupListener = new PopupListener();
+        addMouseListener(popupListener);
+    }
+
     @Override
     public void valueChanged(ListSelectionEvent e) {
         super.valueChanged(e);
         if (!e.getValueIsAdjusting()) {
             int rowIndex = convertRowIndexToModel(getSelectedRow());
             if (rowIndex != -1) {
-                packetViewer.setSelectedPacket((ListPacket)getModel().getValueAt(rowIndex, 3));
+                // Reflect selection to mark/unmark actions
+                if (markedRows.contains(rowIndex))
+                    markPacketMenuItem.setAction(unmarkPacketAction);
+                else
+                    markPacketMenuItem.setAction(markPacketAction);
+                packetViewer.setSelectedPacket((ListPacket)getModel().getValueAt(rowIndex, 2));
             }
         }
     }
@@ -173,45 +257,32 @@ public class PacketsTable extends JTable implements ListSelectionListener, Packe
         // TODO Auto-generated method stub        
     }
 
-    private class TableMouseListener extends MouseAdapter {
+    private class PopupListener extends MouseAdapter {
         @Override
         public void mousePressed(MouseEvent e) {
-            int row = convertRowIndexToModel(rowAtPoint(e.getPoint()));
-            int col = convertColumnIndexToModel(columnAtPoint(e.getPoint()));
+            maybeShowPopup(e);
+        }
 
-            if (col == 0) {            
-                if (ICON_UNSTARRED.equals(getModel().getValueAt(row, col))) {
-                    getModel().setValueAt(ICON_STARRED, row, col);
-                } else {
-                    getModel().setValueAt(ICON_UNSTARRED, row, col);
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            maybeShowPopup(e);
+        }
+
+        private void maybeShowPopup(MouseEvent e) {
+            if (e.isPopupTrigger()) {
+                int row = rowAtPoint(e.getPoint());
+                if (row >= 0 && row < getRowCount())
+                    setRowSelectionInterval(row, row);
+                else
+                    clearSelection();
+
+                if (getSelectedRow() < 0)
+                    return;
+                if (e.isPopupTrigger() && e.getComponent() instanceof JTable) {
+                    popup.show(e.getComponent(), e.getX(), e.getY());
+                    repaint(); // !
                 }
             }
-        }
-    }
-
-    /**
-     * Custom IconRenderer that (as opposed to the default JTable IconRenderer)
-     * prevents visual effectes of cell selection (blue border around Icon)
-     */
-    private static class IconRenderer extends DefaultTableCellRenderer.UIResource {
-        private static final long serialVersionUID = 1L;
-
-        public IconRenderer() {
-            super();
-            setHorizontalAlignment(JLabel.CENTER);
-        }
-
-        @Override
-        public void setValue(Object value) {
-            setIcon((value instanceof Icon) ? (Icon)value : null);
-        }
-
-        @Override
-        public Component getTableCellRendererComponent(JTable table,
-                Object value, boolean isSelected, boolean hasFocus, int row,
-                int column) {
-            return super.getTableCellRendererComponent(table, value,
-                    isSelected, false /* ! */, row, column);
         }
     }
 }
