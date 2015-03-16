eb68df93229d226507c745785c2e3baabef4f828
Fabian Diet <fabian.diet@spaceapplications.com>
2015-03-16 08:01:10 +0100

    OSX: Add support for typical location of libjtokyocabinet

--- yamcs-core/bin/setlibrarypath.sh
+++ yamcs-core/bin/setlibrarypath.sh
@@ -1,6 +1,6 @@
-for d in /usr/lib /usr/lib64 /usr/local/lib /usr/local/lib64;
+for d in /usr/lib /usr/lib64 /usr/local/lib /usr/local/lib64 /opt/local/lib;
 do 
-   if [ -e $d/libjtokyocabinet.so ];
+   if [ -e $d/libjtokyocabinet.so ] || [ -e $d/libjtokyocabinet.dylib ];
    then
       JAVA_LIBRARY_PATH=$JAVA_LIBRARY_PATH:$d   
    fi
