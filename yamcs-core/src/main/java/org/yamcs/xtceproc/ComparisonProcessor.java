60c5d84880c47eb25ff409d186d13500a1140689
Nicolae Mihalache <xpromache@gmail.com>
2015-04-21 14:04:12 +0200

    comply with the last XTCE comparison that uses java Objects instead of primitive data types

--- yamcs-core/src/main/java/org/yamcs/xtceproc/ComparisonProcessor.java
+++ yamcs-core/src/main/java/org/yamcs/xtceproc/ComparisonProcessor.java
@@ -7,12 +7,14 @@ import org.slf4j.LoggerFactory;
 import org.yamcs.ParameterValue;
 import org.yamcs.parameter.ParameterValueList;
 import org.yamcs.protobuf.Yamcs.Value;
+import org.yamcs.protobuf.Yamcs.Value.Type;
 import org.yamcs.xtce.Comparison;
 import org.yamcs.xtce.ComparisonList;
 import org.yamcs.xtce.MatchCriteria;
 import org.yamcs.xtce.ParameterInstanceRef;
 
 import com.google.common.primitives.UnsignedBytes;
+import com.google.common.primitives.UnsignedLongs;
 
 /**
  * Implements XTCE processing for {@link org.yamcs.xtce.Comparison}
@@ -22,177 +24,248 @@ import com.google.common.primitives.UnsignedBytes;
 public class ComparisonProcessor {
     Logger log=LoggerFactory.getLogger(this.getClass().getName());
     final ParameterValueList params;
-
     /**
      * @param params pvals within 'scope'. Identifier resolution is performed against these pvals
      */
     public ComparisonProcessor(ParameterValueList params) {
-	this.params = params;
+        this.params = params;
     }
 
     public boolean matches(MatchCriteria mc) {
-	if(mc instanceof ComparisonList) {
-	    return matchesComparisonList((ComparisonList)mc);
-	} else if(mc instanceof Comparison) {
-	    Comparison c=(Comparison)mc;
-	    switch(c.getValueType()) {
-	    case LONG:
-		return matchesIntegerComparison(c);
-	    case DOUBLE:
-		return matchesFloatComparison(c);
-	    case STRING:
-		return matchesStringComparison(c);
-	    }
-	}
+        if(mc instanceof ComparisonList) {
+            return matchesComparisonList((ComparisonList)mc);
+        } else if(mc instanceof Comparison) {
+            Comparison c=(Comparison)mc;
 
-	throw new UnsupportedOperationException("matching "+mc+" not supported");
+            Object v = c.getValue();
+            if(v instanceof Integer) {
+                return matchesIntegerComparison(c, (Integer)v);
+            } else if(v instanceof Long) {
+                return matchesIntegerComparison(c, (Long) v);
+            } else if (v instanceof Float) {
+                return matchesFloatComparison(c, (Float) v);
+            } else if (v instanceof Double) {
+                return matchesFloatComparison(c, (Double) v);
+            } else if (v instanceof String) {
+                return matchesStringComparison(c, (String)v);
+            } else if (v instanceof byte[]) {
+                return matchesBinaryComparison(c, (byte[])v);
+            }  else if (v instanceof Boolean) {
+                return matchesBooleanComparison(c, (Boolean)v);
+            } else {
+                log.error("checking values of type"+v.getClass()+" not supported");
+                return false;
+            }
+        }
+
+        throw new UnsupportedOperationException("matching "+mc+" not supported");
     }
 
-    private boolean matchesIntegerComparison(Comparison ic) {
-	//Because there is no mechanism in place yet to decide which value of the parameter should be used
-	// in case of multiple values present, traverse the list in reverse order to get the value of the most recently 
-	// extracted parameter first.
-	ParameterInstanceRef paraRef=ic.getParameterRef();
-	ParameterValue pv = params.getLast(paraRef.getParameter());
-	if(pv==null) return false;
+    private boolean matchesIntegerComparison(Comparison ic, long comparisonValue) {
+        //Because there is no mechanism in place yet to decide which value of the parameter should be used
+        // in case of multiple values present, traverse the list in reverse order to get the value of the most recently 
+        // extracted parameter first.
+        ParameterInstanceRef paraRef=ic.getParameterRef();
+        ParameterValue pv = params.getLast(paraRef.getParameter());
+        if(pv==null) return false;
 
-	long value=ic.getLongValue();
-	if(paraRef.useCalibratedValue()) {
-	    switch (ic.getComparisonOperator()) {
-	    case EQUALITY:
-		return (pv.getEngValue().getUint32Value() == value);
-	    case INEQUALITY:
-		return (pv.getEngValue().getUint32Value() != value);
-	    case LARGERTHAN:
-		return (pv.getEngValue().getUint32Value() > value);
-	    case LARGEROREQUALTHAN:
-		return (pv.getEngValue().getUint32Value() >= value);
-	    case SMALLERTHAN:
-		return (pv.getEngValue().getUint32Value() < value);
-	    case SMALLEROREQUALTHAN:
-		return (pv.getEngValue().getUint32Value() <= value);
-	    }
-	} else {
-	    Value pvraw=pv.getRawValue();
-	    long upv;
-	    if(pvraw.getType()==Value.Type.UINT32) {
-		upv=pvraw.getUint32Value() & 0xFFFFFFFFL;
-	    } else {
-		upv=pvraw.getSint32Value();
-	    }
-
-	    switch (ic.getComparisonOperator()) {
-	    case EQUALITY:
-		return (pv.getRawValue().getUint32Value() == value);
-	    case INEQUALITY:
-		return (upv != value);
-	    case LARGERTHAN:
-		return (upv > value);
-	    case LARGEROREQUALTHAN:
-		return (upv >= value);
-	    case SMALLERTHAN:
-		return (upv < value);
-	    case SMALLEROREQUALTHAN:
-		return (upv <= value);
-	    }
-	}
-	return false;
+        Value v;
+        if(paraRef.useCalibratedValue()) {
+            v = pv.getEngValue(); 
+        } else {
+            v = pv.getRawValue();
+        }
+        long pvalue;
+        boolean unsigned;
+        if(v.getType()==Type.SINT32) {
+            pvalue = v.getSint32Value();
+            unsigned = false;
+        } else if(v.getType()==Type.SINT64) {
+            pvalue = v.getSint64Value();
+            unsigned = false;
+        } else if(v.getType()==Type.UINT32) {
+            pvalue = v.getUint32Value() & 0xFFFFFFFFFFFFFFFFL;
+            unsigned = true;
+        } else if(v.getType()==Type.UINT64) {
+            pvalue = v.getUint64Value();
+            unsigned = true;
+        } else {
+            throw new IllegalArgumentException("Cannot compare "+ic.getValue().getClass()+" to "+v.getType());
+        }
+        if(unsigned) {
+            switch (ic.getComparisonOperator()) {
+            case EQUALITY:
+                return (pvalue == comparisonValue);
+            case INEQUALITY:
+                return (pvalue != comparisonValue);
+            case LARGERTHAN:
+                return UnsignedLongs.compare(pvalue, comparisonValue)>0;
+            case LARGEROREQUALTHAN:
+                return UnsignedLongs.compare(pvalue, comparisonValue)>=0;
+            case SMALLERTHAN:
+                return UnsignedLongs.compare(pvalue, comparisonValue)<0;
+            case SMALLEROREQUALTHAN:
+                return UnsignedLongs.compare(pvalue, comparisonValue)<=0;
+            }
+        } else {
+            switch (ic.getComparisonOperator()) {
+            case EQUALITY:
+                return (pvalue == comparisonValue);
+            case INEQUALITY:
+                return (pvalue != comparisonValue);
+            case LARGERTHAN:
+                return (pvalue > comparisonValue);
+            case LARGEROREQUALTHAN:
+                return (pvalue >= comparisonValue);
+            case SMALLERTHAN:
+                return (pvalue < comparisonValue);
+            case SMALLEROREQUALTHAN:
+                return (pvalue <= comparisonValue);
+            }
+        }
+        return false;
     }
 
-    private boolean matchesFloatComparison(Comparison fc) {
-	double value=fc.getDoubleValue();
-	ParameterValue pv= params.getLast(fc.getParameterRef().getParameter());
-	if(pv==null) return false;
+    private boolean matchesFloatComparison(Comparison fc, double comparisonValue) {
+        ParameterValue pv= params.getLast(fc.getParameterRef().getParameter());
+        if(pv==null) return false;
 
-	if(fc.getParameterRef().useCalibratedValue()) {
-	    switch (fc.getComparisonOperator()) {
-	    case EQUALITY:
-		return (pv.getEngValue().getDoubleValue() == value);
-	    case INEQUALITY:
-		return (pv.getEngValue().getDoubleValue() != value);
-	    case LARGERTHAN:
-		return (pv.getEngValue().getDoubleValue() > value);
-	    case LARGEROREQUALTHAN:
-		return (pv.getEngValue().getDoubleValue() >= value);
-	    case SMALLERTHAN:
-		return (pv.getEngValue().getDoubleValue() < value);
-	    case SMALLEROREQUALTHAN:
-		return (pv.getEngValue().getDoubleValue() <= value);
-	    }
-	} else {
-	    double pvd=pv.getRawValue().getDoubleValue();
-	    switch (fc.getComparisonOperator()) {
-	    case EQUALITY:
-		return (pvd == value);
-	    case INEQUALITY:
-		return (pvd != value);
-	    case LARGERTHAN:
-		return (pvd > value);
-	    case LARGEROREQUALTHAN:
-		return (pvd >= value);
-	    case SMALLERTHAN:
-		return (pvd < value);
-	    case SMALLEROREQUALTHAN:
-		return (pvd <= value);
-	    }
-	}
-	return false;
+        double pvalue;
+        Value v;
+        if(fc.getParameterRef().useCalibratedValue()) {
+            v = pv.getEngValue();
+        } else {
+            v = pv.getRawValue();
+        }
+        if(v.getType()==Type.FLOAT) {
+            pvalue = v.getFloatValue();
+        } else if(v.getType()==Type.DOUBLE) {
+            pvalue = v.getDoubleValue();
+        } else {
+            throw new IllegalArgumentException("Cannot compare "+fc.getValue().getClass()+" to "+v.getType());
+        }
+
+        switch (fc.getComparisonOperator()) {
+        case EQUALITY:
+            return (pvalue == comparisonValue);
+        case INEQUALITY:
+            return (pvalue != comparisonValue);
+        case LARGERTHAN:
+            return (pvalue > comparisonValue);
+        case LARGEROREQUALTHAN:
+            return (pvalue >= comparisonValue);
+        case SMALLERTHAN:
+            return (pvalue < comparisonValue);
+        case SMALLEROREQUALTHAN:
+            return (pvalue <= comparisonValue);
+        }
+        return false;
     }
 
 
 
-    private boolean matchesStringComparison(Comparison sc) {//TODO perf
-	//Because there is no mechanism in place yet to decide which value of the parameter should be used
-	// in case of multiple values present, traverse the list in reverse order to get the value of the most recently 
-	// extracted parameter first.
+    private boolean matchesStringComparison(Comparison sc, String svalue) {
+        //Because there is no mechanism in place yet to decide which value of the parameter should be used
+        // in case of multiple values present, traverse the list in reverse order to get the value of the most recently 
+        // extracted parameter first.
 
-	ParameterInstanceRef paraRef=sc.getParameterRef();
-	ParameterValue pv = params.getLast(paraRef.getParameter());
-	if(pv==null) return false;
+        ParameterInstanceRef paraRef=sc.getParameterRef();
+        ParameterValue pv = params.getLast(paraRef.getParameter());
+        if(pv==null) return false;
 
-	if(paraRef.useCalibratedValue()) {
-	    String value=sc.getStringValue();
-	    String s=pv.getEngValue().getStringValue();
-	    switch (sc.getComparisonOperator()) {
-	    case EQUALITY:
-		return (s.compareTo(value) == 0);
-	    case INEQUALITY:
-		return (s.compareTo(value) != 0);
-	    case LARGERTHAN:
-		return (s.compareTo(value) > 0);
-	    case LARGEROREQUALTHAN:
-		return (s.compareTo(value) >= 0);
-	    case SMALLERTHAN:
-		return (s.compareTo(value) < 0);
-	    case SMALLEROREQUALTHAN:
-		return (s.compareTo(value) <= 0);
-	    }
-	} else {
-	    byte[] bvalue=sc.getBinaryValue();
-	    Comparator<byte[]> comparator=UnsignedBytes.lexicographicalComparator();
-	    byte[] pvb=pv.getRawValue().getBinaryValue().toByteArray();
-	    switch (sc.getComparisonOperator()) {
-	    case EQUALITY:
-		return (comparator.compare(pvb, bvalue) == 0);
-	    case INEQUALITY:
-		return (comparator.compare(pvb, bvalue) != 0);
-	    case LARGERTHAN:
-		return (comparator.compare(pvb, bvalue) > 0);
-	    case LARGEROREQUALTHAN:
-		return (comparator.compare(pvb, bvalue) >= 0);
-	    case SMALLERTHAN:
-		return (comparator.compare(pvb, bvalue) < 0);
-	    case SMALLEROREQUALTHAN:
-		return (comparator.compare(pvb, bvalue) <= 0);
-	    }
-	}
-	return false;
+        String pvalue;
+        if(paraRef.useCalibratedValue()) {
+            pvalue = pv.getEngValue().getStringValue();
+        }  else {
+            pvalue = pv.getRawValue().getStringValue();
+        }
+        switch (sc.getComparisonOperator()) {
+        case EQUALITY:
+            return (svalue.compareTo(pvalue) == 0);
+        case INEQUALITY:
+            return (svalue.compareTo(pvalue) != 0);
+        case LARGERTHAN:
+            return (svalue.compareTo(pvalue) > 0);
+        case LARGEROREQUALTHAN:
+            return (svalue.compareTo(pvalue) >= 0);
+        case SMALLERTHAN:
+            return (svalue.compareTo(pvalue) < 0);
+        case SMALLEROREQUALTHAN:
+            return (svalue.compareTo(pvalue) <= 0);
+        }
+
+        return false;
     }
 
+    private boolean matchesBinaryComparison(Comparison sc, byte[] comparisonValue) {
+        ParameterInstanceRef paraRef=sc.getParameterRef();
+        ParameterValue pv = params.getLast(paraRef.getParameter());
+        if(pv==null) return false;
+
+        Comparator<byte[]> comparator=UnsignedBytes.lexicographicalComparator();
+        byte[] pvb;
+        if(paraRef.useCalibratedValue()) {
+            pvb=  pv.getEngValue().getBinaryValue().toByteArray();
+        }  else {
+            pvb=  pv.getRawValue().getBinaryValue().toByteArray();
+        }
+
+
+        switch (sc.getComparisonOperator()) {
+        case EQUALITY:
+            return (comparator.compare(pvb, comparisonValue) == 0);
+        case INEQUALITY:
+            return (comparator.compare(pvb, comparisonValue) != 0);
+        case LARGERTHAN:
+            return (comparator.compare(pvb, comparisonValue) > 0);
+        case LARGEROREQUALTHAN:
+            return (comparator.compare(pvb, comparisonValue) >= 0);
+        case SMALLERTHAN:
+            return (comparator.compare(pvb, comparisonValue) < 0);
+        case SMALLEROREQUALTHAN:
+            return (comparator.compare(pvb, comparisonValue) <= 0);
+        }
+        return false;
+    }
+
+    
+    private boolean matchesBooleanComparison(Comparison bc, boolean comparisonValue) {
+        //Because there is no mechanism in place yet to decide which value of the parameter should be used
+        // in case of multiple values present, traverse the list in reverse order to get the value of the most recently 
+        // extracted parameter first.
+        ParameterInstanceRef paraRef=bc.getParameterRef();
+        ParameterValue pv = params.getLast(paraRef.getParameter());
+        if(pv==null) return false;
+
+        boolean v;
+        if(paraRef.useCalibratedValue()) {
+            v = pv.getEngValue().getBooleanValue(); 
+        } else {
+            v = pv.getRawValue().getBooleanValue();
+        }
+        
+        switch (bc.getComparisonOperator()) {
+        case EQUALITY:
+            return v== comparisonValue;
+        case INEQUALITY:
+            return v!=comparisonValue;
+        case LARGERTHAN:
+            return Boolean.compare(v, comparisonValue) > 0;
+        case LARGEROREQUALTHAN:
+            return Boolean.compare(v, comparisonValue) >= 0;
+        case SMALLERTHAN:
+            return Boolean.compare(v, comparisonValue) < 0;
+        case SMALLEROREQUALTHAN:
+            return Boolean.compare(v, comparisonValue) <= 0;
+        }
+        return false;
+    }
+    
+    
     public boolean matchesComparisonList(ComparisonList cl) {
-	for (Comparison c:cl.getComparisonList()) {
-	    if(!matches(c)) return false;
-	}
-	return true;
+        for (Comparison c:cl.getComparisonList()) {
+            if(!matches(c)) return false;
+        }
+        return true;
     }
 }
