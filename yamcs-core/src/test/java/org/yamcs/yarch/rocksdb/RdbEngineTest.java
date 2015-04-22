c88f5e8c62e3f666ed0a8689ed3843ffa8d399c3
Nicolae Mihalache <xpromache@gmail.com>
2015-04-22 13:48:53 +0200

    cleanup

--- yamcs-core/src/test/java/org/yamcs/yarch/rocksdb/RdbEngineTest.java
+++ yamcs-core/src/test/java/org/yamcs/yarch/rocksdb/RdbEngineTest.java
@@ -19,43 +19,43 @@ import com.google.common.io.Files;
 
 
 public class RdbEngineTest extends YarchTestCase {
-		
-	@Test
-	public void testCreateDrop() throws Exception {
-		RdbStorageEngine rse = new RdbStorageEngine(ydb);
-		
-		 TupleDefinition tdef=new TupleDefinition();
-		 tdef.addColumn(new ColumnDefinition("gentime", DataType.TIMESTAMP));
-		 tdef.addColumn(new ColumnDefinition("packetid", DataType.INT));	        	       
-		 TableDefinition tblDef = new TableDefinition("RdbEngineTest", tdef, Arrays.asList("gentime"));
-		 
-		 
-		 String tmpdir=Files.createTempDir().getAbsolutePath();
-	     tblDef.setDataDir(tmpdir);
-	        
-	        
-		 PartitioningSpec pspec=PartitioningSpec.timeAndValueSpec("gentime", "packetid");
-		 tblDef.setPartitioningSpec(pspec);
-		 IllegalArgumentException iae=null;
-		 try {
-			 rse.newTableReaderStream(tblDef);
-		 } catch (IllegalArgumentException e) {
-			 iae=e;
-		 }
-		 assertNotNull(iae);
-		 		 
-		 rse.createTable(tblDef);
-		 TableWriter tw = rse.newTableWriter(tblDef, InsertMode.INSERT);
-		 Tuple t = new Tuple(tdef, new Object[]{1000L, 10});
-		 tw.onTuple(null, t);		 
-		 rse.dropTable(tblDef);
-		 
-		 iae=null;
-		 try {
-			 rse.newTableReaderStream(tblDef);
-		 } catch (IllegalArgumentException e) {
-			 iae=e;
-		 }
-		 assertNotNull(iae);		 		 		 
-	}
+
+    @Test
+    public void testCreateDrop() throws Exception {
+        RdbStorageEngine rse = new RdbStorageEngine(ydb);
+
+        TupleDefinition tdef=new TupleDefinition();
+        tdef.addColumn(new ColumnDefinition("gentime", DataType.TIMESTAMP));
+        tdef.addColumn(new ColumnDefinition("packetid", DataType.INT));	        	       
+        TableDefinition tblDef = new TableDefinition("RdbEngineTest", tdef, Arrays.asList("gentime"));
+
+
+        String tmpdir=Files.createTempDir().getAbsolutePath();
+        tblDef.setDataDir(tmpdir);
+
+
+        PartitioningSpec pspec=PartitioningSpec.timeAndValueSpec("gentime", "packetid");
+        tblDef.setPartitioningSpec(pspec);
+        IllegalArgumentException iae=null;
+        try {
+            rse.newTableReaderStream(tblDef);
+        } catch (IllegalArgumentException e) {
+            iae=e;
+        }
+        assertNotNull(iae);
+
+        rse.createTable(tblDef);
+        TableWriter tw = rse.newTableWriter(tblDef, InsertMode.INSERT);
+        Tuple t = new Tuple(tdef, new Object[]{1000L, 10});
+        tw.onTuple(null, t);		 
+        rse.dropTable(tblDef);
+
+        iae=null;
+        try {
+            rse.newTableReaderStream(tblDef);
+        } catch (IllegalArgumentException e) {
+            iae=e;
+        }
+        assertNotNull(iae);		 		 		 
+    }
 }
