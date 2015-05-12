74da2d3e6c9a36b96deaf6fc02577bd7e35434d2
Fabian Diet <fabian.diet@spaceapplications.com>
2015-05-12 06:14:53 +0200

    Fix ProcessorRequestHandler not responding
    We should eventually probably improve rest API to more easily catch
    these things, and also to maybe return a default empty response.

--- yamcs-core/src/main/java/org/yamcs/web/rest/ProcessorRequestHandler.java
+++ yamcs-core/src/main/java/org/yamcs/web/rest/ProcessorRequestHandler.java
@@ -4,40 +4,44 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.QueryStringDecoder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yamcs.YProcessor;
 import org.yamcs.YamcsException;
 import org.yamcs.management.ManagementService;
+import org.yamcs.protobuf.Rest.RestConnectToProcessorResponse;
+import org.yamcs.protobuf.Rest.RestCreateProcessorResponse;
+import org.yamcs.protobuf.Rest.RestProcessorResponse;
+import org.yamcs.protobuf.SchemaRest;
 import org.yamcs.protobuf.SchemaYamcsManagement;
 import org.yamcs.protobuf.YamcsManagement.ProcessorManagementRequest;
 import org.yamcs.protobuf.YamcsManagement.ProcessorRequest;
 import org.yamcs.security.AuthenticationToken;
+import org.yamcs.security.Privilege;
 
 public class ProcessorRequestHandler extends AbstractRestRequestHandler {
     private static final Logger log = LoggerFactory.getLogger(ProcessorRequestHandler.class.getName());
     
     
     public void handleRequest(ChannelHandlerContext ctx, FullHttpRequest req, String yamcsInstance, String remainingUri, AuthenticationToken authToken) throws RestException {
-        String[] path = remainingUri.split("/", 2);
-        
-        if (path.length == 0) {
-            handleProcessorManagementRequest(ctx, req, yamcsInstance, authToken);
+        QueryStringDecoder qsDecoder = new QueryStringDecoder(remainingUri);
+        if ("".equals(qsDecoder.path())) {
+            handleProcessorManagementRequest(ctx, req, yamcsInstance, qsDecoder, authToken);
         } else {
-            String yprocName = path[0];
+            String yprocName = remainingUri.split("/")[0];
             YProcessor yproc = YProcessor.getInstance(yamcsInstance, yprocName);
             if(yproc==null) {
                 log.warn("Sending NOT_FOUND because invalid processor name '{}' has been requested", yprocName);
                 sendError(ctx, HttpResponseStatus.NOT_FOUND);
                 return;
             }
-            remainingUri = remainingUri.substring(path.length > 1 ? path[0].length() + 1 : path[0].length());
-            handleProcessorRequest(ctx, req, yproc, remainingUri);
+            handleProcessorRequest(ctx, req, yproc, qsDecoder);
         }
     }
         
-    private void handleProcessorRequest(ChannelHandlerContext ctx, FullHttpRequest req, YProcessor yproc, String remainingUri) throws RestException {
+    private void handleProcessorRequest(ChannelHandlerContext ctx, FullHttpRequest req, YProcessor yproc, QueryStringDecoder qsDecoder) throws RestException {
         ProcessorRequest yprocReq = readMessage(req, SchemaYamcsManagement.ProcessorRequest.MERGE).build();
         if(req.getMethod()==HttpMethod.POST) {
             switch(yprocReq.getOperation()) {
@@ -65,10 +69,12 @@ public class ProcessorRequestHandler extends AbstractRestRequestHandler {
             default:
                 throw new BadRequestException("Invalid operation "+yprocReq.getOperation()+" specified");
             }
+            RestProcessorResponse response = RestProcessorResponse.newBuilder().build();
+            writeMessage(ctx, req, qsDecoder, response, SchemaRest.RestProcessorResponse.WRITE);
         }
     }
     
-    private void handleProcessorManagementRequest(ChannelHandlerContext ctx, FullHttpRequest req, String yamcsInstance, AuthenticationToken authToken) throws RestException {
+    private void handleProcessorManagementRequest(ChannelHandlerContext ctx, FullHttpRequest req, String yamcsInstance, QueryStringDecoder qsDecoder, AuthenticationToken authToken) throws RestException {
         ProcessorManagementRequest yprocReq = readMessage(req, SchemaYamcsManagement.ProcessorManagementRequest.MERGE).build();
         if(req.getMethod()==HttpMethod.POST) {
             switch(yprocReq.getOperation()) {
@@ -76,6 +82,8 @@ public class ProcessorRequestHandler extends AbstractRestRequestHandler {
                 ManagementService mservice = ManagementService.getInstance();
                 try {
                     mservice.connectToProcessor(yprocReq, authToken);
+                    RestConnectToProcessorResponse response = RestConnectToProcessorResponse.newBuilder().build();
+                    writeMessage(ctx, req, qsDecoder, response, SchemaRest.RestConnectToProcessorResponse.WRITE);
                 } catch (YamcsException e) {
                     throw new BadRequestException(e.getMessage());
                 }
@@ -85,6 +93,8 @@ public class ProcessorRequestHandler extends AbstractRestRequestHandler {
                 mservice = ManagementService.getInstance();
                 try {
                     mservice.createProcessor(yprocReq, authToken);
+                    RestCreateProcessorResponse response = RestCreateProcessorResponse.newBuilder().build();
+                    writeMessage(ctx, req, qsDecoder, response, SchemaRest.RestCreateProcessorResponse.WRITE);
                 } catch (YamcsException e) {
                     throw new BadRequestException(e.getMessage());
                 }
