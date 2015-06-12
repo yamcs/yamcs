0fc613454a37d8dea6d37d225d4c01cb03396bec
Nicolae Mihalache <xpromache@gmail.com>
2015-06-12 17:12:07 +0200

    fixed the static file transfer

--- yamcs-core/src/main/java/org/yamcs/web/StaticFileRequestHandler.java
+++ yamcs-core/src/main/java/org/yamcs/web/StaticFileRequestHandler.java
@@ -5,34 +5,46 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import javax.activation.MimetypesFileTypeMap;
 
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelProgressiveFuture;
+import io.netty.channel.ChannelProgressiveFutureListener;
+import io.netty.channel.DefaultFileRegion;
 import io.netty.handler.codec.http.DefaultHttpResponse;
 import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpHeaders.Names;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.LastHttpContent;
+import io.netty.handler.ssl.SslHandler;
 import io.netty.handler.stream.ChunkedFile;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yamcs.ConfigurationException;
 import org.yamcs.YConfiguration;
 
-import static io.netty.handler.codec.http.HttpHeaders.isKeepAlive;
-import static io.netty.handler.codec.http.HttpHeaders.setContentLength;
 import static io.netty.handler.codec.http.HttpResponseStatus.FORBIDDEN;
 import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
+
 public class StaticFileRequestHandler extends AbstractRequestHandler {
     public static String WEB_Root;
     static MimetypesFileTypeMap mimeTypesMap;
+    public static final int HTTP_CACHE_SECONDS = 60;
+    
     final static Logger log=LoggerFactory.getLogger(StaticFileRequestHandler.class.getName());
     
     public static void init() throws ConfigurationException {
@@ -47,6 +59,7 @@ public class StaticFileRequestHandler extends AbstractRequestHandler {
     }
     
     void handleStaticFileRequest(ChannelHandlerContext ctx, HttpRequest req, String path) throws Exception {
+        log.debug("handling static file request for {}", path);
         path = sanitizePath(path);
         if (path == null) {
             sendError(ctx, FORBIDDEN);
@@ -82,51 +95,97 @@ public class StaticFileRequestHandler extends AbstractRequestHandler {
         RandomAccessFile raf;
         try {
             raf = new RandomAccessFile(file, "r");
-        } catch (FileNotFoundException fnfe) {
+        } catch (FileNotFoundException ignore) {
             sendError(ctx, NOT_FOUND);
             return;
         }
         long fileLength = raf.length();
 
         HttpResponse response = new DefaultHttpResponse(HTTP_1_1, OK);
-        setContentLength(response, fileLength);
-        setContentTypeHeader(response, mimeTypesMap.getContentType(file.getPath()));
-        setDateAndCacheHeaders(response, new Date(file.lastModified()));
-        
+        HttpHeaders.setContentLength(response, fileLength);
+        setContentTypeHeader(response, file);
+        setDateAndCacheHeaders(response, file);
+        if (HttpHeaders.isKeepAlive(req)) {
+            response.headers().set(Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+        }
 
         // Write the initial line and the header.
         ctx.write(response);
 
         // Write the content.
-        ChannelFuture writeFuture;
-        //if (ch.getPipeline().get(SslHandler.class) != null) {
-            // Cannot use zero-copy with HTTPS.
-            writeFuture = ctx.write(new ChunkedFile(raf, 0, fileLength, 8192));
-       /* } else {
-            // No encryption - use zero-copy.
-            final FileRegion region =  new DefaultFileRegion(raf.getChannel(), 0, fileLength);
-            writeFuture = ch.write(region);
-            writeFuture.addListener(new ChannelFutureProgressListener() {
-                @Override
-                public void operationComplete(ChannelFuture future) {
-                    region.releaseExternalResources();
-                }
+        ChannelFuture sendFileFuture;
+        ChannelFuture lastContentFuture;
+        if (ctx.pipeline().get(SslHandler.class) == null) {
+            sendFileFuture = ctx.write(new DefaultFileRegion(raf.getChannel(), 0, fileLength), ctx.newProgressivePromise());
+            // Write the end marker.
+            lastContentFuture = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+        } else {
+            sendFileFuture = ctx.writeAndFlush(new ChunkedFile(raf, 0, fileLength, 8192),  ctx.newProgressivePromise());
+            // HttpChunkedInput will write the end marker (LastHttpContent) for us.
+            lastContentFuture = sendFileFuture;
+        }
 
-                @Override
-                public void operationProgressed(ChannelFuture future, long amount, long current, long total) {
-                    System.out.printf("%s: %d / %d (+%d)%n", path, current, total, amount);
+        sendFileFuture.addListener(new ChannelProgressiveFutureListener() {
+            @Override
+            public void operationProgressed(ChannelProgressiveFuture future, long progress, long total) {
+                if (total < 0) { // total unknown
+                    System.err.println(future.channel() + " Transfer progress: " + progress);
+                } else {
+                    System.err.println(future.channel() + " Transfer progress: " + progress + " / " + total);
                 }
-            });
-        }*/
+            }
+
+            @Override
+            public void operationComplete(ChannelProgressiveFuture future) {
+                System.err.println(future.channel() + " Transfer complete.");
+            }
+        });
 
         // Decide whether to close the connection or not.
-        if (!isKeepAlive(req)) {
+        if (!HttpHeaders.isKeepAlive(req)) {
             // Close the connection when the whole content is written out.
-            writeFuture.addListener(ChannelFutureListener.CLOSE);
+            lastContentFuture.addListener(ChannelFutureListener.CLOSE);
         }
     }
     
         
+    /**
+     * Sets the content type header for the HTTP Response
+     *
+     * @param response
+     *            HTTP response
+     * @param file
+     *            file to extract content type
+     */
+    private static void setContentTypeHeader(HttpResponse response, File file) {
+        MimetypesFileTypeMap mimeTypesMap = new MimetypesFileTypeMap();
+        response.headers().set(Names.CONTENT_TYPE, mimeTypesMap.getContentType(file.getPath()));
+    }
+    
+    /**
+     * Sets the Date and Cache headers for the HTTP Response
+     *
+     * @param response
+     *            HTTP response
+     * @param fileToCache
+     *            file to extract content type
+     */
+    private static void setDateAndCacheHeaders(HttpResponse response, File fileToCache) {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat(HTTP_DATE_FORMAT, Locale.US);
+        dateFormatter.setTimeZone(TimeZone.getTimeZone(HTTP_DATE_GMT_TIMEZONE));
+
+        // Date header
+        Calendar time = new GregorianCalendar();
+        response.headers().set(Names.DATE, dateFormatter.format(time.getTime()));
+
+        // Add cache headers
+        time.add(Calendar.SECOND, HTTP_CACHE_SECONDS);
+        response.headers().set(Names.EXPIRES, dateFormatter.format(time.getTime()));
+        response.headers().set(Names.CACHE_CONTROL, "private, max-age=" + HTTP_CACHE_SECONDS);
+        response.headers().set(Names.LAST_MODIFIED, dateFormatter.format(new Date(fileToCache.lastModified())));
+    }
+
+    
     /**
      * When file timestamp is the same as what the browser is sending up, send a "304 Not Modified"
      * 
