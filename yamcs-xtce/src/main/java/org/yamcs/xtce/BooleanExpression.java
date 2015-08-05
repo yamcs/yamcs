0e2689b36434a911c58aa6e51c0656cc0d970018
dhosa <dho@spaceapplications.com>
2015-08-05 15:59:08 +0200

    Add BooleanExpression

---
+++ yamcs-xtce/src/main/java/org/yamcs/xtce/BooleanExpression.java
@@ -0,0 +1,14 @@
+package org.yamcs.xtce;
+
+import java.util.Set;
+
+public class BooleanExpression implements MatchCriteria {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Set<Parameter> getDependentParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
