b0b04c5244f017cc328cdedd3102ab8c7de66890
Nicolae Mihalache <xpromache@gmail.com>
2015-09-11 16:26:59 +0200

    test for the storageEngine serialization

--- yamcs-core/src/test/java/org/yamcs/yarch/TableDefinitionSerializationTest.java
+++ yamcs-core/src/test/java/org/yamcs/yarch/TableDefinitionSerializationTest.java
@@ -85,7 +85,7 @@ public class TableDefinitionSerializationTest extends YarchTestCase {
         assertDefEquals("aav2", DataType.BINARY, td2.getColumnDefinition("aav2"));
         assertDefEquals("aav3", DataType.ENUM, td2.getColumnDefinition("aav3"));
         assertDefEquals("bbv1", DataType.DOUBLE, td2.getColumnDefinition("bbv1"));
-        assertEquals(ydb.getDefaultStorageEngine(), td2.getStorageEngineName());
+        assertEquals(ydb.getDefaultStorageEngineName(), td2.getStorageEngineName());
         
         assertTrue(td2.hasHistogram());
         List<String> al=td2.getHistogramColumns();
@@ -164,7 +164,7 @@ public class TableDefinitionSerializationTest extends YarchTestCase {
         assertDefEquals("aav2", DataType.BINARY, td2.getColumnDefinition("aav2"));
         assertDefEquals("aav3", DataType.ENUM, td2.getColumnDefinition("aav3"));
         assertDefEquals("bbv1", DataType.DOUBLE, td2.getColumnDefinition("bbv1"));
-        assertEquals(ydb.getDefaultStorageEngine(), td2.getStorageEngineName());
+        assertEquals(ydb.getDefaultStorageEngineName(), td2.getStorageEngineName());
         
         assertTrue(td2.hasHistogram());
         List<String> al=td2.getHistogramColumns();
