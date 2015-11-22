60e240338c90c751233d29d209dcd52c6279ba4e
Fabian Diet <fabian.diet@spaceapplications.com>
2015-11-22 22:57:50 +0100

    Make sure subscriptions from top bar are running

--- yamcs-web/src/intf/intf.module.js
+++ yamcs-web/src/intf/intf.module.js
@@ -2,5 +2,9 @@
     'use strict';
 
     angular
-        .module('yamcs.intf', []);
+        .module('yamcs.intf', [])
+        .run(function (alarmsService, eventsService) {
+            // Eagerly loading these injected services, to get the subscriptions running
+            // Perhaps should research whether we should use angular providers instead.
+        });
 })();
