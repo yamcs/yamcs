3c5cded1dd14ee19cf5a11a7cf0ed872c54fe7e7
Fabian Diet <fabian.diet@spaceapplications.com>
2015-11-24 22:02:18 +0100

    Enable filtering archived alarms

--- yamcs-web/src/alarms/pages/archived-alarms.js
+++ yamcs-web/src/alarms/pages/archived-alarms.js
@@ -14,9 +14,16 @@
 
         // Alarms sorted by key. Combines state of realtime and history
         vm.alarms = [];
-
         alarmsService.listAlarms().then(function (alarms) {
             vm.alarms = alarms;
         });
+
+        vm.searchText = '';
+        vm.filterTable = function(value) {
+            if (!vm.searchText) return true;
+            var qname = value['triggerValue']['id']['name'];
+            var regex = new RegExp(vm.searchText.trim().replace('*', '.*'), 'i');
+            return regex.test(qname);
+        };
     }
 })();
