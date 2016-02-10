39114bdcfdb5a2ddfed1235313bfc103fc06ab45
Nicolae Mihalache <xpromache@gmail.com>
2016-02-10 17:09:23 +0100

    disable debugging in the integration test

--- yamcs-core/src/test/java/org/yamcs/AbstractIntegrationTest.java
+++ yamcs-core/src/test/java/org/yamcs/AbstractIntegrationTest.java
@@ -68,7 +68,7 @@ public abstract class AbstractIntegrationTest {
 
     @BeforeClass
     public static void beforeClass() throws Exception {
-        enableDebugging();
+       // enableDebugging();
         setupYamcs();
     }
 
