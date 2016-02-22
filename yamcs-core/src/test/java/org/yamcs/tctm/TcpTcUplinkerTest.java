9ae58bace77bf2afd26556459a9eaf5b3130b758
Nicolae Mihalache <xpromache@gmail.com>
2016-02-22 14:46:18 +0100

    forgotten test case for minimumTcPacketLength in the TcpTcUplinker

--- yamcs-core/src/test/java/org/yamcs/tctm/TcpTcUplinkerTest.java
+++ yamcs-core/src/test/java/org/yamcs/tctm/TcpTcUplinkerTest.java
@@ -8,11 +8,11 @@ public class TcpTcUplinkerTest {
     @Test
     public void testConfig() {
         TcpTcUplinker tcuplink = new TcpTcUplinker("testinst", "name0", "test_default");
+        assertEquals(-1, tcuplink.getMiniminimumTcPacketLength());
+        
+        
+        tcuplink = new TcpTcUplinker("testinst", "test1", "test48");
         assertEquals(48, tcuplink.getMiniminimumTcPacketLength());
-        
-        
-        tcuplink = new TcpTcUplinker("testinst", "test1", "test0");
-        assertEquals(0, tcuplink.getMiniminimumTcPacketLength());
         
     }
 }
