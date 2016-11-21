1de9897292f7928b5b930613bcacc3f71a2b5384
Nicolae Mihalache <xpromache@gmail.com>
2016-11-21 12:55:46 +0100

    updated artemis to latest version + some cleanup

--- yamcs-core/src/main/java/org/yamcs/hornetq/ArtemisServer.java
+++ yamcs-core/src/main/java/org/yamcs/hornetq/ArtemisServer.java
@@ -1,40 +1,11 @@
 package org.yamcs.hornetq;
 
-import static org.yamcs.api.artemis.Protocol.DATA_TO_HEADER_NAME;
-import static org.yamcs.api.artemis.Protocol.LINK_CONTROL_ADDRESS;
-import static org.yamcs.api.artemis.Protocol.LINK_INFO_ADDRESS;
-import static org.yamcs.api.artemis.Protocol.REPLYTO_HEADER_NAME;
-import static org.yamcs.api.artemis.Protocol.REQUEST_TYPE_HEADER_NAME;
-
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-
-import org.apache.activemq.artemis.api.core.ActiveMQException;
-import org.apache.activemq.artemis.api.core.Message;
-import org.apache.activemq.artemis.api.core.SimpleString;
-import org.apache.activemq.artemis.api.core.client.ClientMessage;
-import org.apache.activemq.artemis.api.core.client.MessageHandler;
 import org.apache.activemq.artemis.core.server.embedded.EmbeddedActiveMQ;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yamcs.ConfigurationException;
 import org.yamcs.YConfiguration;
-import org.yamcs.YamcsException;
-import org.yamcs.YamcsServer;
-import org.yamcs.api.YamcsApiException;
-import org.yamcs.api.artemis.Protocol;
-import org.yamcs.api.artemis.YamcsClient;
-import org.yamcs.api.artemis.YamcsSession;
-import org.yamcs.management.LinkListener;
-import org.yamcs.management.ManagementService;
-import org.yamcs.protobuf.YamcsManagement.LinkInfo;
-import org.yamcs.protobuf.YamcsManagement.MissionDatabaseRequest;
 import org.yamcs.security.HornetQAuthManager;
-import org.yamcs.security.HqClientMessageToken;
-import org.yamcs.security.Privilege;
-import org.yamcs.utils.ActiveMQBufferOutputStream;
-import org.yamcs.xtce.XtceDb;
-import org.yamcs.xtceproc.XtceDbFactory;
 
 import com.google.common.util.concurrent.AbstractService;
 
