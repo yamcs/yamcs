f6f5f8b6c1880d873c332ed72485bac39d053101
Nicolae Mihalache <xpromache@gmail.com>
2017-01-11 19:36:49 +0100

    fixed reference after removing obsolete class

--- yamcs-core/src/test/java/org/yamcs/tctm/TcpTcUplinkerTest.java
+++ yamcs-core/src/test/java/org/yamcs/tctm/TcpTcUplinkerTest.java
@@ -7,11 +7,11 @@ import org.junit.Test;
 public class TcpTcUplinkerTest {
     @Test
     public void testConfig() {
-        TcpTcUplinker tcuplink = new TcpTcUplinker("testinst", "name0", "test_default");
+        TcpTcDataLink tcuplink = new TcpTcDataLink("testinst", "name0", "test_default");
         assertEquals(-1, tcuplink.getMiniminimumTcPacketLength());
         
         
-        tcuplink = new TcpTcUplinker("testinst", "test1", "test48");
+        tcuplink = new TcpTcDataLink("testinst", "test1", "test48");
         assertEquals(48, tcuplink.getMiniminimumTcPacketLength());
         
     }
