0a380b8103ccd6581d2e9c99ad98241f8b71f9dc
Fabian Diet <fabian.diet@spaceapplications.com>
2017-10-27 12:27:52 +0200

    Extract YSS devmode to separate script

--- make-live-devel.sh
+++ make-live-devel.sh
@@ -34,29 +34,6 @@ ln -fs $YAMCS_HOME/yamcs-web/build $TARGET/web/base
 
 if [ -f make-live-devel-local.sh ] ; then
     sh make-live-devel-local.sh $TARGET
-else
-    # Assume YSS simulator deployment
-    cp -an $YAMCS_HOME/yamcs-simulation/etc/* $TARGET/etc
-    cp -an $YAMCS_HOME/yamcs-simulation/bin/* $TARGET/bin
-
-    ln -fs $YAMCS_HOME/yamcs-simulation/target/*.jar $TARGET/lib
-    ln -fs $YAMCS_HOME/yamcs-simulation/mdb/* $TARGET/mdb
-    ln -fs $YAMCS_HOME/yamcs-simulation/web $TARGET/web/yss
-
-    cp -an $YAMCS_HOME/yamcs-simulation/bin/simulator.sh $TARGET/bin
-    ln -fs $YAMCS_HOME/yamcs-simulation/test_data $TARGET/
-    YAMCS_DATA=/storage/yamcs-data/
-    # create the yamcs_data directory and add the simulator profiles
-    mkdir -p $YAMCS_DATA/simulator/profiles
-    if [ $? -ne 0 ]; then
-        echo "ERROR: could not create $YAMCS_DATA/simulator/profiles - please create it and make sure this script has write permissions in it!"
-        exit 1
-    fi
-    if [ ! -w "$YAMCS_DATA/simulator/profiles" ]; then
-        echo "ERROR: please make sure this script has write permissions in the $YAMCS_DATA/simulator/profiles folder!"
-        exit 2
-    fi
-    cp -an $YAMCS_HOME/yamcs-simulation/profiles/* $YAMCS_DATA/simulator/profiles
 fi
 
 # Add sample config (if not already present)
---
+++ setup-example.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Sets up a development environment for an example Yamcs configuration.
+
+YAMCS_DATA=/storage/yamcs-data/
+
+. "$PWD/make-live-devel.sh" example
+
+ln -fs $YAMCS_HOME/yamcs-simulation/etc/* $TARGET/etc
+ln -fs $YAMCS_HOME/yamcs-simulation/bin/* $TARGET/bin
+
+ln -fs $YAMCS_HOME/yamcs-simulation/target/*.jar $TARGET/lib
+ln -fs $YAMCS_HOME/yamcs-simulation/mdb/* $TARGET/mdb
+ln -fs $YAMCS_HOME/yamcs-simulation/web $TARGET/web/yss
+
+ln -fs $YAMCS_HOME/yamcs-simulation/bin/simulator.sh $TARGET/bin
+ln -fs $YAMCS_HOME/yamcs-simulation/test_data $TARGET/
+
+ln -fs $YAMCS_HOME/yamcs-simulation/profiles $YAMCS_DATA/simulator
+if [ $? -ne 0 ]; then
+    echo "ERROR: could not create $YAMCS_DATA/simulator/profiles - please create it and make sure this script has write permissions in it!"
+    exit 1
+fi
+if [ ! -w "$YAMCS_DATA/simulator/profiles" ]; then
+    echo "ERROR: please make sure this script has write permissions in the $YAMCS_DATA/simulator/profiles folder!"
+    exit 2
+fi
+
+echo "Example created at `ls -d -1 $PWD/example`"
