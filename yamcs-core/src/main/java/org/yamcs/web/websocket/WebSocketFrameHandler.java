db86e6c56f813cad3fa7da3d18284361e962bb7b
Nicolae Mihalache <xpromache@gmail.com>
2017-10-31 09:36:02 +0100

    added some web socket settings to help with dropped messages and connections

--- yamcs-core/src/main/java/org/yamcs/web/websocket/WebSocketFrameHandler.java
+++ yamcs-core/src/main/java/org/yamcs/web/websocket/WebSocketFrameHandler.java
@@ -2,7 +2,6 @@ package org.yamcs.web.websocket;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +16,7 @@ import org.yamcs.security.AuthenticationToken;
 import org.yamcs.web.HttpRequestHandler;
 import org.yamcs.web.HttpRequestInfo;
 import org.yamcs.web.HttpServer;
+import org.yamcs.web.WebConfig;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufInputStream;
@@ -60,15 +60,19 @@ public class WebSocketFrameHandler extends SimpleChannelInboundHandler<WebSocket
     // Provides access to the various resources served through this websocket
     private Map<String, AbstractWebSocketResource> resourcesByName = new HashMap<>();
 
+    //after how many consecutive dropped writes will the connection be closed
+    
+    int connectionCloseNumDroppedMsg;
     public WebSocketFrameHandler(HttpRequestInfo originalRequestInfo) {
         this.originalRequestInfo = originalRequestInfo;
+        connectionCloseNumDroppedMsg = WebConfig.getInstance().getWebSocketConnectionCloseNumDroppedMsg();
     }
 
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
         this.ctx = ctx;
         channel = ctx.channel();
-
+        channel.config().setWriteBufferWaterMark(WebConfig.getInstance().getWebSocketWriteBufferWaterMark());
         // Try to use an application name as provided by the client. For browsers (since overriding
         // websocket headers is not supported) this will be the browser's standard user-agent string
         String applicationName;
@@ -212,9 +216,9 @@ public class WebSocketFrameHandler extends SimpleChannelInboundHandler<WebSocket
                     dataType, processorClient.getClientId(), processorClient.getUsername());
             droppedWrites++;
 
-            if (droppedWrites == 5) {
-                log.warn("Too many failed writes for client [id={}, username={}]. Forcing disconnect",
-                        processorClient.getClientId(), processorClient.getUsername());
+            if (droppedWrites >= connectionCloseNumDroppedMsg) {
+                log.warn("Too many ({}) dropped messages for client [id={}, username={}]. Forcing disconnect",
+                        droppedWrites, processorClient.getClientId(), processorClient.getUsername());
                 ctx.close();
             }
             return;
