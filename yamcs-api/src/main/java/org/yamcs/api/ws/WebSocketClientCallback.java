0cfc5a914b86854d77eecd081c811f89c6e45274
Fabian Diet <fabian.diet@spaceapplications.com>
2017-12-16 23:42:30 +0100

    Add 'connecting' callback to WebSocketClient

--- yamcs-api/src/main/java/org/yamcs/api/ws/WebSocketClientCallback.java
+++ yamcs-api/src/main/java/org/yamcs/api/ws/WebSocketClientCallback.java
@@ -5,24 +5,33 @@ import org.yamcs.protobuf.Yamcs.NamedObjectId;
 
 public interface WebSocketClientCallback {
 
-    
+    /**
+     * When a connection attempt is underway
+     */
+    default void connecting() {
+    }
+
     /**
      * When the connection was successfully established
      */
-    default void connected() {}
-    
+    default void connected() {
+    }
+
     /**
      * When the initial connection attempt failed
      */
-    default void connectionFailed(Throwable t) {}
-    
+    default void connectionFailed(Throwable t) {
+    }
+
     /**
      * When a previously successful connection was disconnected
      */
-    default void disconnected() {}
-    
+    default void disconnected() {
+    }
+
     // TODO get this out of here and into the WebSocketResponseHandler
-    default void onInvalidIdentification(NamedObjectId id) {}
-    
+    default void onInvalidIdentification(NamedObjectId id) {
+    }
+
     void onMessage(WebSocketSubscriptionData data);
 }
