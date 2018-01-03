77add1fb24b34ac0c4b5de65ec1e1025ddcea24e
Nicolae Mihalache <xpromache@gmail.com>
2018-01-03 16:41:08 +0100

    changed the unsubscribeAll to unsubscribe both the "all" subscription as well as the individual subscriptions added an opsname and a script such that the example in the api doc works

---
+++ yamcs-api/src/test/python/subscribe-params-docs.py
@@ -0,0 +1,18 @@
+import websocket
+ws = websocket.WebSocket()
+wsaddr = "ws://localhost:8090/simulator/_websocket"
+print "Connecting to "+wsaddr + "...",
+ws.connect(wsaddr)
+print "connected"
+
+print " subcribing to parameters...",
+
+ws.send('[1,1,3, {"parameter": "subscribe", "data": { "id" : [ \
+           {"name": "/YSS/SIMULATOR/BatteryVoltage1"},\
+           { "namespace": "MDB:OPS Name", "name": "SIMULATOR_BatteryVoltage2" }\
+        ], "updateOnExpiration": true }}]')
+print "subscribed"
+while True:
+    result =  ws.recv()
+    print "Received '%s'" % result
+
