4234f955b35583b01f0e99dd79b795dcb8b6a974
Nicolae Mihalache <xpromache@gmail.com>
2018-02-27 11:01:09 +0100

    added an example for sending a command with curl

---
+++ yamcs-api/src/test/curl/issue-command.sh
@@ -0,0 +1,8 @@
+curl -XPOST http://localhost:8090/api/processors/simulator/realtime/commands/YSS/SIMULATOR/SWITCH_VOLTAGE_ON -d '{
+  "sequenceNumber" : 1,
+  "origin" : "nico",
+  "assignment" : [ {
+    "name": "voltage_num",
+    "value": "3"
+  } ]
+}'
