19481625b8dc92ecf7a2394afd4e91f5481fb665
Fabian Diet <fabian.diet@spaceapplications.com>
2018-03-07 12:52:53 +0100

    Clean-up unused script

--- yamcs-simulation/web/simulator/displays/scripts/ReadFileExample.js
+++
@@ -1,7 +0,0 @@
-importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
-
-var filePath = display.getWidget("filePath").getPropertyValue("text");
-
-var text = FileUtil.readTextFile(filePath);
-
-display.getWidget("readLabel").setPropertyValue("text", text);
