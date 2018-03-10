d605954f7ba3ba0f570b5112097689077d721760
Fabian Diet <fabian.diet@spaceapplications.com>
2018-03-10 17:38:09 +0100

    Add polyfill for fetch api

--- yamcs-web/packages/app/src/polyfills.ts
+++ yamcs-web/packages/app/src/polyfills.ts
@@ -41,6 +41,9 @@ import 'core-js/es6/set';
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
 
+// Promises + Fetch API
+import 'core-js/fn/promise';
+import 'whatwg-fetch';
 
 /**
  * Required to support Web Animations `@angular/platform-browser/animations`.
