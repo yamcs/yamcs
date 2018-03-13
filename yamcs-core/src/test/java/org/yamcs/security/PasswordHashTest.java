322455c4ac6e9af90c7a7b70f528865fcd983e99
Nicolae Mihalache <xpromache@gmail.com>
2018-03-13 16:30:57 +0100

    cleanup

--- yamcs-core/src/test/java/org/yamcs/security/PasswordHashTest.java
+++ yamcs-core/src/test/java/org/yamcs/security/PasswordHashTest.java
@@ -13,25 +13,20 @@ public class PasswordHashTest {
     @Test
     public void hash_validate_ok() throws InvalidKeySpecException, NoSuchAlgorithmException {
 
-                // Print out 10 hashes
-            for(int i = 0; i < 10; i++)
-                System.out.println(PasswordHash.createHash("p\r\nassw0Rd!"));
+        // Test password validation
+        System.out.println("Running tests...");
+        for (int i = 0; i < 100; i++) {
+            String password = "" + i;
+            String hash = PasswordHash.createHash(password);
+            String secondHash = PasswordHash.createHash(password);
+            Assert.assertFalse("Two hashes should not be equal", hash.equals(secondHash));
 
-            // Test password validation
-            boolean failure = false;
-            System.out.println("Running tests...");
-            for(int i = 0; i < 100; i++)
-            {
-                String password = ""+i;
-                String hash = PasswordHash.createHash(password);
-                String secondHash = PasswordHash.createHash(password);
-                Assert.assertFalse("Two hashes should not be equal", hash.equals(secondHash));
+            String wrongPassword = "" + (i + 1);
+            Assert.assertFalse("Wrong password should not be accepted",
+                    (PasswordHash.validatePassword(wrongPassword, hash)));
 
-                String wrongPassword = ""+(i+1);
-                Assert.assertFalse("Wrong password should not be accepted", (PasswordHash.validatePassword(wrongPassword, hash)));
-
-                Assert.assertTrue("Good password should be accepted", PasswordHash.validatePassword(password, hash));
-            }
+            Assert.assertTrue("Good password should be accepted", PasswordHash.validatePassword(password, hash));
         }
+    }
 
 }
