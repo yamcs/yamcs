151ce1503531c89e23ee76877062eb72d5456dbd
Fabian Diet <fabian.diet@spaceapplications.com>
2018-03-24 13:54:09 +0100

    Add space system link in mdb detail pages

--- yamcs-web/packages/app/src/app/mdb/space-system-detail/SpaceSystemPage.ts
+++ yamcs-web/packages/app/src/app/mdb/space-system-detail/SpaceSystemPage.ts
@@ -9,7 +9,6 @@ import { YamcsService } from '../../core/services/YamcsService';
 import { Store } from '@ngrx/store';
 import { State } from '../../app.reducers';
 import { selectCurrentInstance } from '../../core/store/instance.selectors';
-import { LocationStrategy } from '@angular/common';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -29,7 +28,6 @@ export class SpaceSystemPage implements OnInit {
     yamcs: YamcsService,
     title: Title,
     private store: Store<State>,
-    private url: LocationStrategy,
   ) {
     this.qualifiedName = route.snapshot.paramMap.get('qualifiedName')!;
     this.spaceSystem$ = yamcs.getSelectedInstance().getSpaceSystem(this.qualifiedName);
@@ -38,10 +36,5 @@ export class SpaceSystemPage implements OnInit {
 
   ngOnInit() {
     this.instance$ = this.store.select(selectCurrentInstance);
-  }
-
-  isDetailPageOf(segment: string) {
-    const currentUrl = this.url.path().split('?')[0];
-    return currentUrl.indexOf(segment) !== -1 && !currentUrl.endsWith(segment);
   }
 }
