0aa39f9d85f816e12b45c33a7571e19ad4ef174a
Fabian Diet <fabian.diet@spaceapplications.com>
2018-03-24 13:53:20 +0100

    Add instances table

--- yamcs-web/packages/app/src/app/core/pages/HomePage.ts
+++ yamcs-web/packages/app/src/app/core/pages/HomePage.ts
@@ -1,21 +1,50 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewChild, AfterViewInit } from '@angular/core';
 import { Instance } from '@yamcs/client';
 import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
 import { selectInstances } from '../store/instance.selectors';
 import { State } from '../../app.reducers';
 import { Title } from '@angular/platform-browser';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 
 @Component({
   templateUrl: './HomePage.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+
+  @ViewChild(MatSort)
+  sort: MatSort;
+
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
 
   instances$: Observable<Instance[]>;
 
-  constructor(store: Store<State>, title: Title) {
+  dataSource = new MatTableDataSource<Instance>([]);
+
+  displayedColumns = [
+    'name',
+  ];
+
+  constructor(private store: Store<State>, title: Title) {
     title.setTitle('Yamcs');
     this.instances$ = store.select(selectInstances);
+
+    this.dataSource.filterPredicate = (instance, filter) => {
+      return instance.name.toLowerCase().indexOf(filter) >= 0;
+    };
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.store.select(selectInstances).subscribe(instances => {
+      this.dataSource.data = instances;
+    });
+  }
+
+  applyFilter(value: string) {
+    this.dataSource.filter = value.trim().toLowerCase();
   }
 }
