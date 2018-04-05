e5e974c9758f95a605291f36ab4c3ed1f62060de
Nicolae Mihalache <xpromache@gmail.com>
2018-04-05 17:44:36 +0200

    subscribe to the stream in the init method such that when the data links start sending it works no matter in which order services are started in yamcs

--- yamcs-core/src/main/java/org/yamcs/tctm/StreamParameterProvider.java
+++ yamcs-core/src/main/java/org/yamcs/tctm/StreamParameterProvider.java
@@ -36,32 +36,34 @@ public class StreamParameterProvider extends AbstractService implements StreamSu
     ParameterListener paraListener;
     final XtceDb xtceDb;
     private static final Logger log = LoggerFactory.getLogger(StreamParameterProvider.class);
-    
-    ParameterTypeProcessor ptypeProcessor;
-    
-    public StreamParameterProvider(String archiveInstance, Map<String, String> config) throws ConfigurationException {
-        YarchDatabaseInstance ydb=YarchDatabase.getInstance(archiveInstance);
 
-        if(!config.containsKey("stream")) {
-            throw new ConfigurationException("the config(args) for StreamPpProvider has to contain a parameter 'stream' - stream name for retrieving parameters from");
+    ParameterTypeProcessor ptypeProcessor;
+
+    public StreamParameterProvider(String archiveInstance, Map<String, String> config) throws ConfigurationException {
+        YarchDatabaseInstance ydb = YarchDatabase.getInstance(archiveInstance);
+
+        if (!config.containsKey("stream")) {
+            throw new ConfigurationException("the config(args) for StreamPpProvider has to contain a parameter"
+                    + " 'stream' - stream name for retrieving parameters from");
         }
         String streamName = config.get("stream");
 
-        stream=ydb.getStream(streamName);
-        if(stream==null) {
-            throw new ConfigurationException("Cannot find a stream named "+streamName);
+        stream = ydb.getStream(streamName);
+        if (stream == null) {
+            throw new ConfigurationException("Cannot find a stream named " + streamName);
         }
-        xtceDb=XtceDbFactory.getInstance(archiveInstance);
+        xtceDb = XtceDbFactory.getInstance(archiveInstance);
     }
+
     @Override
     public void init(Processor processor) {
         ptypeProcessor = processor.getProcessorData().getParameterTypeProcessor();
         processor.getParameterRequestManager().addParameterProvider(this);
+        stream.addSubscriber(this);
     }
 
     @Override
     protected void doStart() {
-        stream.addSubscriber(this);
         notifyStarted();
     }
 
@@ -75,24 +77,25 @@ public class StreamParameterProvider extends AbstractService implements StreamSu
      * Make sure all parameters are defined in the XtceDB, otherwise the PRM will choke
      */
     @Override
-    public void onTuple(Stream s, Tuple tuple) {//the definition of the tuple is in PpProviderAdapter
-        List<ParameterValue> params=new ArrayList<>();
-        for(int i=4; i<tuple.size(); i++) {
+    public void onTuple(Stream s, Tuple tuple) {// the definition of the tuple is in PpProviderAdapter
+        List<ParameterValue> params = new ArrayList<>();
+        for (int i = 4; i < tuple.size(); i++) {
             Object o = tuple.getColumn(i);
             ParameterValue pv;
-            if(o instanceof org.yamcs.protobuf.Pvalue.ParameterValue) {
-                org.yamcs.protobuf.Pvalue.ParameterValue gpv=(org.yamcs.protobuf.Pvalue.ParameterValue)tuple.getColumn(i);
+            if (o instanceof org.yamcs.protobuf.Pvalue.ParameterValue) {
+                org.yamcs.protobuf.Pvalue.ParameterValue gpv = (org.yamcs.protobuf.Pvalue.ParameterValue) tuple
+                        .getColumn(i);
                 String name = tuple.getColumnDefinition(i).getName();
                 Parameter ppdef = xtceDb.getParameter(name);
-                if(ppdef==null) {
+                if (ppdef == null) {
                     continue;
                 }
                 pv = ParameterValue.fromGpb(ppdef, gpv);
-            } else if(o instanceof ParameterValue) {
-                pv = (ParameterValue)o; 
-                if(pv.getParameter()==null) {
+            } else if (o instanceof ParameterValue) {
+                pv = (ParameterValue) o;
+                if (pv.getParameter() == null) {
                     Parameter ppdef = xtceDb.getParameter(pv.getParameterQualifiedNamed());
-                    if(ppdef==null) {
+                    if (ppdef == null) {
                         continue;
                     }
                     pv.setParameter(ppdef);
@@ -101,9 +104,9 @@ public class StreamParameterProvider extends AbstractService implements StreamSu
                 log.warn("Recieved data that is not parameter value but {}", o.getClass());
                 continue;
             }
-            
-            if(pv.getEngValue()==null &&pv.getRawValue()!=null) {
-                ptypeProcessor.calibrate(pv);                
+
+            if (pv.getEngValue() == null && pv.getRawValue() != null) {
+                ptypeProcessor.calibrate(pv);
             }
             params.add(pv);
         }
@@ -114,7 +117,6 @@ public class StreamParameterProvider extends AbstractService implements StreamSu
     public void streamClosed(Stream s) {
         notifyStopped();
     }
-
 
     @Override
     public void setParameterListener(ParameterListener paraListener) {
@@ -128,7 +130,7 @@ public class StreamParameterProvider extends AbstractService implements StreamSu
 
     @Override
     public boolean canProvide(NamedObjectId id) {
-        if(xtceDb.getParameter(id)!=null) {
+        if (xtceDb.getParameter(id) != null) {
             return true;
         } else {
             return false;
@@ -137,30 +139,26 @@ public class StreamParameterProvider extends AbstractService implements StreamSu
 
     @Override
     public boolean canProvide(Parameter p) {
-        return xtceDb.getParameter(p.getQualifiedName())!=null;
+        return xtceDb.getParameter(p.getQualifiedName()) != null;
     }
 
     @Override
     public Parameter getParameter(NamedObjectId id) throws InvalidIdentification {
-        Parameter p=xtceDb.getParameter(id);
-        if(p==null) {
+        Parameter p = xtceDb.getParameter(id);
+        if (p == null) {
             throw new InvalidIdentification();
-        }
-        else return p;
+        } else
+            return p;
     }
-
 
     @Override
     public void startProviding(Parameter paramDef) {
         // TODO Auto-generated method stub
     }
 
-
     @Override
     public void startProvidingAll() {
         // TODO Auto-generated method stub
     }
 
-
-   
 }
