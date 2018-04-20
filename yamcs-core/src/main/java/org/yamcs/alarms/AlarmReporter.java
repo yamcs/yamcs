e4cd399ae8749107f7d00903fd0ba366a195df7b
Fabian Diet <fabian.diet@spaceapplications.com>
2018-04-21 00:11:02 +0200

    AlarmReporter: Do not use 'type', and use new severities
    Previously type was used to store the 6 XTCE severities. Now that
    event severities also use the XTCE severities, this information has
    become redundant.

--- yamcs-core/src/main/java/org/yamcs/alarms/AlarmReporter.java
+++ yamcs-core/src/main/java/org/yamcs/alarms/AlarmReporter.java
@@ -1,6 +1,12 @@
 package org.yamcs.alarms;
 
-import com.google.common.util.concurrent.AbstractService;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.yamcs.ConfigurationException;
 import org.yamcs.Processor;
 import org.yamcs.api.EventProducer;
@@ -10,77 +16,81 @@ import org.yamcs.parameter.ParameterRequestManager;
 import org.yamcs.parameter.ParameterValue;
 import org.yamcs.protobuf.Pvalue.MonitoringResult;
 import org.yamcs.protobuf.Pvalue.RangeCondition;
-import org.yamcs.xtce.*;
+import org.yamcs.xtce.AlarmReportType;
+import org.yamcs.xtce.AlarmType;
+import org.yamcs.xtce.Parameter;
+import org.yamcs.xtce.ParameterType;
+import org.yamcs.xtce.XtceDb;
 import org.yamcs.xtceproc.XtceDbFactory;
 
-import java.util.*;
+import com.google.common.util.concurrent.AbstractService;
 
 /**
- * Generates realtime alarm events automatically, by subscribing to all relevant
- * parameters.
+ * Generates realtime alarm events automatically, by subscribing to all relevant parameters.
  */
 public class AlarmReporter extends AbstractService implements ParameterConsumer {
-    
+
     private EventProducer eventProducer;
-    private Map<Parameter, ActiveAlarm> activeAlarms=new HashMap<Parameter, ActiveAlarm>();
+    private Map<Parameter, ActiveAlarm> activeAlarms = new HashMap<>();
     // Last value of each param (for detecting changes in value)
-    private Map<Parameter, ParameterValue> lastValuePerParameter=new HashMap<Parameter, ParameterValue>();
+    private Map<Parameter, ParameterValue> lastValuePerParameter = new HashMap<>();
     final String yamcsInstance;
     final String yprocName;
-    
+
     public AlarmReporter(String yamcsInstance) {
         this(yamcsInstance, "realtime");
     }
-    
+
     public AlarmReporter(String yamcsInstance, String yprocName) {
-    	this.yamcsInstance = yamcsInstance;
-    	this.yprocName = yprocName;    			
-        eventProducer=EventProducerFactory.getEventProducer(yamcsInstance);
+        this.yamcsInstance = yamcsInstance;
+        this.yprocName = yprocName;
+        eventProducer = EventProducerFactory.getEventProducer(yamcsInstance);
         eventProducer.setSource("AlarmChecker");
     }
-    
+
     @Override
     public void doStart() {
-    	 Processor yproc = Processor.getInstance(yamcsInstance, yprocName);
-    	 if(yproc==null) {
-    		 ConfigurationException e = new ConfigurationException("Cannot find a yproc '"+yprocName+"' in instance '"+yamcsInstance+"'");
-    		 notifyFailed(e);
-    		 return;
-    	 }
-         ParameterRequestManager prm = yproc.getParameterRequestManager();
-         prm.getAlarmChecker().enableReporting(this);
-         
-         // Auto-subscribe to parameters with alarms
-         Set<Parameter> requiredParameters=new HashSet<Parameter>();
-         try {
-             XtceDb xtcedb=XtceDbFactory.getInstance(yamcsInstance);
-             for (Parameter parameter:xtcedb.getParameters()) {
-                 ParameterType ptype=parameter.getParameterType();
-                 if(ptype!=null && ptype.hasAlarm()) {
-                     requiredParameters.add(parameter);
-                     Set<Parameter> dependentParameters = ptype.getDependentParameters();
-                     if(dependentParameters!=null) {
-                         requiredParameters.addAll(dependentParameters);
-                     }
-                 }
-             }
-         } catch(ConfigurationException e) {
-        	 notifyFailed(e);
-    		 return;
-         }
-         
-         if(!requiredParameters.isEmpty()) {
-             List<Parameter> params=new ArrayList<Parameter>(requiredParameters); // Now that we have uniques..
-             prm.addRequest(params, this);
-         }
+        Processor yproc = Processor.getInstance(yamcsInstance, yprocName);
+        if (yproc == null) {
+            ConfigurationException e = new ConfigurationException(
+                    "Cannot find a yproc '" + yprocName + "' in instance '" + yamcsInstance + "'");
+            notifyFailed(e);
+            return;
+        }
+        ParameterRequestManager prm = yproc.getParameterRequestManager();
+        prm.getAlarmChecker().enableReporting(this);
+
+        // Auto-subscribe to parameters with alarms
+        Set<Parameter> requiredParameters = new HashSet<>();
+        try {
+            XtceDb xtcedb = XtceDbFactory.getInstance(yamcsInstance);
+            for (Parameter parameter : xtcedb.getParameters()) {
+                ParameterType ptype = parameter.getParameterType();
+                if (ptype != null && ptype.hasAlarm()) {
+                    requiredParameters.add(parameter);
+                    Set<Parameter> dependentParameters = ptype.getDependentParameters();
+                    if (dependentParameters != null) {
+                        requiredParameters.addAll(dependentParameters);
+                    }
+                }
+            }
+        } catch (ConfigurationException e) {
+            notifyFailed(e);
+            return;
+        }
+
+        if (!requiredParameters.isEmpty()) {
+            List<Parameter> params = new ArrayList<>(requiredParameters); // Now that we have uniques..
+            prm.addRequest(params, this);
+        }
         notifyStarted();
     }
-    
+
     @Override
     public void doStop() {
         notifyStopped();
     }
-    
+
     @Override
     public void updateItems(int subscriptionId, List<ParameterValue> items) {
         // Nothing. The real business of sending events, happens while checking the alarms
@@ -89,168 +99,189 @@ public class AlarmReporter extends AbstractService implements ParameterConsumer 
     }
 
     /**
-     * Sends an event if an alarm condition for the active context has been
-     * triggered <tt>minViolations</tt> times. This configuration does not
-     * affect events for parameters that go back to normal, or that change
-     * severity levels while the alarm is already active.
+     * Sends an event if an alarm condition for the active context has been triggered <tt>minViolations</tt> times. This
+     * configuration does not affect events for parameters that go back to normal, or that change severity levels while
+     * the alarm is already active.
      */
     public void reportNumericParameterEvent(ParameterValue pv, AlarmType alarmType, int minViolations) {
-        boolean sendUpdateEvent=false;
+        boolean sendUpdateEvent = false;
 
-        if(alarmType == null)
-        {
+        if (alarmType == null) {
             // TODO: do something with more interesting
             return;
         }
-        
-        if(alarmType.getAlarmReportType()==AlarmReportType.ON_VALUE_CHANGE) {
-            ParameterValue oldPv=lastValuePerParameter.get(pv.getParameter());
-            if(oldPv!=null && hasChanged(oldPv, pv)) {
-                sendUpdateEvent=true;
+
+        if (alarmType.getAlarmReportType() == AlarmReportType.ON_VALUE_CHANGE) {
+            ParameterValue oldPv = lastValuePerParameter.get(pv.getParameter());
+            if (oldPv != null && hasChanged(oldPv, pv)) {
+                sendUpdateEvent = true;
             }
             lastValuePerParameter.put(pv.getParameter(), pv);
         }
-        
-        if(pv.getMonitoringResult()==MonitoringResult.IN_LIMITS) {
-            if(activeAlarms.containsKey(pv.getParameter())) {
-                eventProducer.sendInfo("NORMAL", "Parameter "+pv.getParameter().getQualifiedName()+" is back to normal");
+
+        if (pv.getMonitoringResult() == MonitoringResult.IN_LIMITS) {
+            if (activeAlarms.containsKey(pv.getParameter())) {
+                eventProducer.sendInfo(null,
+                        "Parameter " + pv.getParameter().getQualifiedName() + " is back to normal");
                 activeAlarms.remove(pv.getParameter());
             }
         } else { // out of limits
-            MonitoringResult previousMonitoringResult=null;
-            ActiveAlarm activeAlarm=activeAlarms.get(pv.getParameter());
-            if(activeAlarm==null || activeAlarm.alarmType!=alarmType) {
-                activeAlarm=new ActiveAlarm(alarmType, pv.getMonitoringResult());
+            MonitoringResult previousMonitoringResult = null;
+            ActiveAlarm activeAlarm = activeAlarms.get(pv.getParameter());
+            if (activeAlarm == null || activeAlarm.alarmType != alarmType) {
+                activeAlarm = new ActiveAlarm(alarmType, pv.getMonitoringResult());
             } else {
-                previousMonitoringResult=activeAlarm.monitoringResult;
-                activeAlarm.monitoringResult=pv.getMonitoringResult();
+                previousMonitoringResult = activeAlarm.monitoringResult;
+                activeAlarm.monitoringResult = pv.getMonitoringResult();
                 activeAlarm.violations++;
             }
-            
-            if(activeAlarm.violations==minViolations || (activeAlarm.violations>minViolations && previousMonitoringResult!=activeAlarm.monitoringResult)) {
-                sendUpdateEvent=true;
+
+            if (activeAlarm.violations == minViolations || (activeAlarm.violations > minViolations
+                    && previousMonitoringResult != activeAlarm.monitoringResult)) {
+                sendUpdateEvent = true;
             }
-            
+
             activeAlarms.put(pv.getParameter(), activeAlarm);
         }
-        
-        if(sendUpdateEvent) {
+
+        if (sendUpdateEvent) {
             sendValueChangeEvent(pv);
         }
     }
-    
-    public void reportEnumeratedParameterEvent(ParameterValue pv, AlarmType alarmType, int minViolations) {
-        boolean sendUpdateEvent=false;
 
-        if(alarmType == null) {
+    public void reportEnumeratedParameterEvent(ParameterValue pv, AlarmType alarmType, int minViolations) {
+        boolean sendUpdateEvent = false;
+
+        if (alarmType == null) {
             // TODO: something more interesting
             return;
         }
 
-        if(alarmType.getAlarmReportType()==AlarmReportType.ON_VALUE_CHANGE) {
-            ParameterValue oldPv=lastValuePerParameter.get(pv.getParameter());
-            if(oldPv!=null && hasChanged(oldPv, pv)) {
-                sendUpdateEvent=true;
+        if (alarmType.getAlarmReportType() == AlarmReportType.ON_VALUE_CHANGE) {
+            ParameterValue oldPv = lastValuePerParameter.get(pv.getParameter());
+            if (oldPv != null && hasChanged(oldPv, pv)) {
+                sendUpdateEvent = true;
             }
             lastValuePerParameter.put(pv.getParameter(), pv);
         }
-        
-        if(pv.getMonitoringResult()==MonitoringResult.IN_LIMITS) {
-            if(activeAlarms.containsKey(pv.getParameter())) {
-                eventProducer.sendInfo("NORMAL", "Parameter "+pv.getParameter().getQualifiedName()+" is back to a normal state ("+pv.getEngValue().getStringValue()+")");
+
+        if (pv.getMonitoringResult() == MonitoringResult.IN_LIMITS) {
+            if (activeAlarms.containsKey(pv.getParameter())) {
+                eventProducer.sendInfo(null, "Parameter " + pv.getParameter().getQualifiedName()
+                        + " is back to a normal state (" + pv.getEngValue().getStringValue() + ")");
                 activeAlarms.remove(pv.getParameter());
             }
         } else { // out of limits
-            MonitoringResult previousMonitoringResult=null;
-            ActiveAlarm activeAlarm=activeAlarms.get(pv.getParameter());
-            if(activeAlarm==null || activeAlarm.alarmType!=alarmType) {
-                activeAlarm=new ActiveAlarm(alarmType, pv.getMonitoringResult());
+            MonitoringResult previousMonitoringResult = null;
+            ActiveAlarm activeAlarm = activeAlarms.get(pv.getParameter());
+            if (activeAlarm == null || activeAlarm.alarmType != alarmType) {
+                activeAlarm = new ActiveAlarm(alarmType, pv.getMonitoringResult());
             } else {
-                previousMonitoringResult=activeAlarm.monitoringResult;
-                activeAlarm.monitoringResult=pv.getMonitoringResult();
+                previousMonitoringResult = activeAlarm.monitoringResult;
+                activeAlarm.monitoringResult = pv.getMonitoringResult();
                 activeAlarm.violations++;
             }
-            
-            if(activeAlarm.violations==minViolations || (activeAlarm.violations>minViolations&& previousMonitoringResult!=activeAlarm.monitoringResult)) {
-                sendUpdateEvent=true;
+
+            if (activeAlarm.violations == minViolations || (activeAlarm.violations > minViolations
+                    && previousMonitoringResult != activeAlarm.monitoringResult)) {
+                sendUpdateEvent = true;
             }
-            
+
             activeAlarms.put(pv.getParameter(), activeAlarm);
         }
-        
-        if(sendUpdateEvent) {
+
+        if (sendUpdateEvent) {
             sendStateChangeEvent(pv);
         }
     }
-    
+
     private void sendValueChangeEvent(ParameterValue pv) {
-        if(pv.getMonitoringResult() == null)
-        {
-            eventProducer.sendInfo("no monitoring", "Parameter "+pv.getParameter().getQualifiedName()+" has changed to value "+pv.getEngValue());
+        if (pv.getMonitoringResult() == null) {
+            eventProducer.sendInfo(null,
+                    "Parameter " + pv.getParameter().getQualifiedName() + " has changed to value " + pv.getEngValue());
             return;
         }
-        
+
         if (pv.getMonitoringResult() == MonitoringResult.IN_LIMITS) {
-            eventProducer.sendInfo(pv.getMonitoringResult().toString(), "Parameter "+pv.getParameter().getQualifiedName()+" has changed to value "+pv.getEngValue());
+            eventProducer.sendInfo(null,
+                    "Parameter " + pv.getParameter().getQualifiedName() + " has changed to value " + pv.getEngValue());
         } else {
             String message;
             if (pv.getRangeCondition() == RangeCondition.LOW) {
-                message = "Parameter "+pv.getParameter().getQualifiedName()+" is too low";
+                message = "Parameter " + pv.getParameter().getQualifiedName() + " is too low";
             } else if (pv.getRangeCondition() == RangeCondition.HIGH) {
-                message = "Parameter "+pv.getParameter().getQualifiedName()+" is too high";
+                message = "Parameter " + pv.getParameter().getQualifiedName() + " is too high";
             } else {
-                throw new IllegalStateException("Unexpected range condition: "+pv.getRangeCondition());                
+                throw new IllegalStateException("Unexpected range condition: " + pv.getRangeCondition());
             }
-            
+
             switch (pv.getMonitoringResult()) {
             case WATCH:
+                eventProducer.sendWatch(null, message);
+                break;
             case WARNING:
-                eventProducer.sendWarning(pv.getMonitoringResult().toString(), message);
+                eventProducer.sendWarning(null, message);
                 break;
             case DISTRESS:
+                eventProducer.sendDistress(null, message);
+                break;
             case CRITICAL:
+                eventProducer.sendCritical(null, message);
+                break;
             case SEVERE:
-                eventProducer.sendSevere(pv.getMonitoringResult().toString(), message);
+                eventProducer.sendSevere(null, message);
                 break;
             default:
-                throw new IllegalStateException("Unexpected monitoring result: "+pv.getMonitoringResult());
+                throw new IllegalStateException("Unexpected monitoring result: " + pv.getMonitoringResult());
             }
         }
     }
-    
+
     private void sendStateChangeEvent(ParameterValue pv) {
-        switch(pv.getMonitoringResult()) {
+        switch (pv.getMonitoringResult()) {
         case WATCH:
+            eventProducer.sendWatch(null, "Parameter " + pv.getParameter().getQualifiedName()
+                    + " transitioned to state " + pv.getEngValue().getStringValue());
+            break;
         case WARNING:
-            eventProducer.sendWarning(pv.getMonitoringResult().toString(), "Parameter "+pv.getParameter().getQualifiedName()+" transitioned to state "+pv.getEngValue().getStringValue());
+            eventProducer.sendWarning(null, "Parameter " + pv.getParameter().getQualifiedName()
+                    + " transitioned to state " + pv.getEngValue().getStringValue());
             break;
         case DISTRESS:
+            eventProducer.sendDistress(null, "Parameter " + pv.getParameter().getQualifiedName()
+                    + " transitioned to state " + pv.getEngValue().getStringValue());
+            break;
         case CRITICAL:
+            eventProducer.sendCritical(null, "Parameter " + pv.getParameter().getQualifiedName()
+                    + " transitioned to state " + pv.getEngValue().getStringValue());
+            break;
         case SEVERE:
-            eventProducer.sendSevere(pv.getMonitoringResult().toString(), "Parameter "+pv.getParameter().getQualifiedName()+" transitioned to state "+pv.getEngValue().getStringValue());
+            eventProducer.sendSevere(null, "Parameter " + pv.getParameter().getQualifiedName()
+                    + " transitioned to state " + pv.getEngValue().getStringValue());
             break;
         case IN_LIMITS:
-            eventProducer.sendInfo(pv.getMonitoringResult().toString(), "Parameter "+pv.getParameter().getQualifiedName()+" transitioned to state "+pv.getEngValue().getStringValue());
+            eventProducer.sendInfo(null, "Parameter " + pv.getParameter().getQualifiedName() + " transitioned to state "
+                    + pv.getEngValue().getStringValue());
             break;
         default:
-            throw new IllegalStateException("Unexpected monitoring result: "+pv.getMonitoringResult());
+            throw new IllegalStateException("Unexpected monitoring result: " + pv.getMonitoringResult());
         }
     }
-    
+
     private boolean hasChanged(ParameterValue pvOld, ParameterValue pvNew) {
         // Crude string value comparison.
         return !pvOld.getEngValue().equals(pvNew.getEngValue());
     }
-    
+
     private static class ActiveAlarm {
         MonitoringResult monitoringResult;
         AlarmType alarmType;
-        int violations=1;
+        int violations = 1;
         ParameterValue lastValue;
-        
+
         ActiveAlarm(AlarmType alarmType, MonitoringResult monitoringResult) {
-            this.alarmType=alarmType;
-            this.monitoringResult=monitoringResult;
+            this.alarmType = alarmType;
+            this.monitoringResult = monitoringResult;
         }
     }
 }
