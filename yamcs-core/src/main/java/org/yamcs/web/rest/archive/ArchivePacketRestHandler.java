9beb4e46f7fef3e7957e1ebc5995c8b639e01422
Fabian Diet <fabian.diet@spaceapplications.com>
2018-04-26 16:11:45 +0200

    Add packet index request operation
    We already had this under downloads, but this one has limit options.

--- yamcs-core/src/main/java/org/yamcs/web/rest/archive/ArchivePacketRestHandler.java
+++ yamcs-core/src/main/java/org/yamcs/web/rest/archive/ArchivePacketRestHandler.java
@@ -2,17 +2,32 @@ package org.yamcs.web.rest.archive;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import org.yamcs.YamcsException;
+import org.yamcs.YamcsServer;
 import org.yamcs.api.MediaType;
 import org.yamcs.archive.GPBHelper;
+import org.yamcs.archive.IndexRequestListener;
+import org.yamcs.archive.IndexServer;
 import org.yamcs.archive.XtceTmRecorder;
+import org.yamcs.protobuf.Archive.IndexEntry;
+import org.yamcs.protobuf.Archive.IndexGroup;
+import org.yamcs.protobuf.Archive.IndexResponse;
 import org.yamcs.protobuf.Rest.ListPacketsResponse;
+import org.yamcs.protobuf.Yamcs.ArchiveRecord;
+import org.yamcs.protobuf.Yamcs.IndexRequest;
+import org.yamcs.protobuf.Yamcs.IndexResult;
+import org.yamcs.protobuf.Yamcs.NamedObjectId;
 import org.yamcs.protobuf.Yamcs.TmPacketData;
 import org.yamcs.security.Privilege;
 import org.yamcs.security.PrivilegeType;
+import org.yamcs.utils.TimeEncoding;
+import org.yamcs.web.BadRequestException;
 import org.yamcs.web.HttpException;
 import org.yamcs.web.InternalServerErrorException;
 import org.yamcs.web.NotFoundException;
@@ -30,6 +45,81 @@ import io.netty.buffer.ByteBuf;
 import io.netty.handler.codec.http.HttpResponseStatus;
 
 public class ArchivePacketRestHandler extends RestHandler {
+
+    @Route(path = "/api/archive/:instance/packet-index", method = "GET")
+    public void listPacketIndex(RestRequest req) throws HttpException {
+        String instance = verifyInstance(req, req.getRouteParam("instance"));
+        IndexServer indexServer = verifyIndexServer(req, instance);
+
+        int limit = req.getQueryParameterAsInt("limit", 100);
+        int mergeTime = req.getQueryParameterAsInt("mergeTime", 2000);
+
+        IndexRequest.Builder requestb = IndexRequest.newBuilder();
+        requestb.setInstance(instance);
+        requestb.setMergeTime(mergeTime);
+        IntervalResult ir = req.scanForInterval();
+        if (ir.hasStart()) {
+            requestb.setStart(ir.getStart());
+        }
+        if (ir.hasStop()) {
+            requestb.setStop(ir.getStop());
+        }
+
+        if (req.hasQueryParameter("name")) {
+            for (String names : req.getQueryParameterList("name")) {
+                for (String name : names.split(",")) {
+                    requestb.addTmPacket(NamedObjectId.newBuilder().setName(name.trim()));
+                }
+            }
+        }
+        if (requestb.getTmPacketCount() == 0) {
+            requestb.setSendAllTm(true);
+        }
+
+        try {
+            IndexResponse.Builder responseb = IndexResponse.newBuilder();
+            Map<NamedObjectId, IndexGroup.Builder> groupBuilders = new HashMap<>();
+            indexServer.submitIndexRequest(requestb.build(), new IndexRequestListener() {
+
+                int count = 0;
+
+                @Override
+                public void processData(IndexResult indexResult) {
+                    if (count < limit) {
+                        for (ArchiveRecord rec : indexResult.getRecordsList()) {
+                            if (count < limit) {
+                                IndexGroup.Builder groupb = groupBuilders.get(rec.getId());
+                                if (groupb == null) {
+                                    groupb = IndexGroup.newBuilder().setId(rec.getId());
+                                    groupBuilders.put(rec.getId(), groupb);
+                                }
+                                groupb.addEntry(IndexEntry.newBuilder()
+                                        .setStart(TimeEncoding.toString(rec.getFirst()))
+                                        .setStop(TimeEncoding.toString(rec.getLast()))
+                                        .setN(rec.getNum()));
+                                count++;
+                            }
+                        }
+                    }
+                }
+
+                @Override
+                public void finished(boolean success) {
+                    if (success) {
+                        groupBuilders.forEach((id, groupb) -> {
+                            responseb.addGroup(groupb);
+                        });
+                        completeOK(req, responseb.build());
+                    } else {
+                        sendRestError(req, HttpResponseStatus.INTERNAL_SERVER_ERROR,
+                                new InternalServerErrorException("Too many results"));
+                    }
+                }
+            });
+        } catch (YamcsException e) {
+            throw new InternalServerErrorException("Too many results", e);
+        }
+    }
 
     @Route(path = "/api/archive/:instance/packets/:gentime?", method = "GET")
     public void listPackets(RestRequest req) throws HttpException {
@@ -130,5 +220,15 @@ public class ArchivePacketRestHandler extends RestHandler {
 
             }
         });
+    }
+
+    private IndexServer verifyIndexServer(RestRequest req, String instance) throws HttpException {
+        verifyInstance(req, instance);
+        IndexServer indexServer = YamcsServer.getService(instance, IndexServer.class);
+        if (indexServer == null) {
+            throw new BadRequestException("Index service not enabled for instance '" + instance + "'");
+        } else {
+            return indexServer;
+        }
     }
 }
