3891510e11c634af0fab598d02d69ab2ce7e2f9c
Fabian Diet <fabian.diet@spaceapplications.com>
2018-06-04 14:45:42 +0200

    Fix CLI tables list

--- yamcs-core/src/main/java/org/yamcs/cli/TablesCli.java
+++ yamcs-core/src/main/java/org/yamcs/cli/TablesCli.java
@@ -15,6 +15,8 @@ import org.yamcs.api.YamcsApiException;
 import org.yamcs.api.YamcsConnectionProperties;
 import org.yamcs.api.rest.BulkRestDataSender;
 import org.yamcs.api.rest.RestClient;
+import org.yamcs.protobuf.Archive.TableInfo;
+import org.yamcs.protobuf.Rest.ListTablesResponse;
 import org.yamcs.protobuf.Table.Row;
 import org.yamcs.protobuf.Table.TableLoadResponse;
 
@@ -53,9 +55,12 @@ public class TablesCli extends Command {
         public void execute() throws Exception {
             YamcsConnectionProperties ycp = getYamcsConnectionProperties();
             RestClient restClient = new RestClient(ycp);
-            String resp = new String(
-                    restClient.doRequest("/api/archive/" + ycp.getInstance() + "/tables", HttpMethod.GET).get());
-            console.print(resp);
+
+            byte[] resp = restClient.doRequest("/archive/" + ycp.getInstance() + "/tables", HttpMethod.GET).get();
+            ListTablesResponse response = ListTablesResponse.parseFrom(resp);
+            for (TableInfo table : response.getTableList()) {
+                console.println(table.getName());
+            }
         }
     }
 
@@ -103,7 +108,7 @@ public class TablesCli extends Command {
                 cf.get();
                 restClient.close();
             }
-            
+
             System.out.println("\rsaved " + count.get() + " rows");
         }
 
