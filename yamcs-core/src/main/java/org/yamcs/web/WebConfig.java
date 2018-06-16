79d0c87ab3961ec8c132ca270daf4a60c71ac6c0
Fabian Diet <fabian.diet@spaceapplications.com>
2018-06-16 08:45:09 +0200

    Remove support for 2-year old deprecated web config

--- yamcs-core/src/main/java/org/yamcs/web/WebConfig.java
+++ yamcs-core/src/main/java/org/yamcs/web/WebConfig.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.yamcs.ConfigurationException;
 import org.yamcs.YConfiguration;
 
@@ -20,7 +18,6 @@ import io.netty.handler.codec.http.cors.CorsConfigBuilder;
  */
 public class WebConfig {
 
-    private static final Logger log = LoggerFactory.getLogger(WebConfig.class);
     private static WebConfig INSTANCE = new WebConfig();
 
     private int port;
@@ -44,30 +41,6 @@ public class WebConfig {
 
     private WebConfig() {
         YConfiguration yconf = YConfiguration.getConfiguration("yamcs");
-
-        if (yconf.containsKey("webPort")) {
-            log.warn(
-                    "Property 'webPort' in yamcs.yaml is deprecated. Instead nest new property 'port' under 'webConfig'.");
-            port = yconf.getInt("webPort");
-        }
-        if (yconf.containsKey("webRoot")) {
-            log.warn("Property 'webRoot' in yamcs.yaml is deprecated. Instead nest 'webRoot' under 'webConfig'.");
-            if (yconf.isList("webRoot")) {
-                @SuppressWarnings({ "unchecked", "rawtypes" })
-                List<String> rootConf = (List) yconf.getList("webRoot");
-                for (String root : rootConf) {
-                    webRoots.add(root);
-                }
-            } else {
-                webRoots.add(yconf.getString("webRoot"));
-            }
-        }
-        if (yconf.containsKey("zeroCopyEnabled")) {
-            log.warn(
-                    "Property 'zeroCopyEnabled' in yamcs.yaml is deprecated. Instead nest 'zeroCopyEnabled' under 'webConfig'.");
-            zeroCopyEnabled = yconf.getBoolean("zeroCopyEnabled");
-        }
-
         CorsConfigBuilder corsb = null;
         if (yconf.containsKey("webConfig")) {
             Map<String, Object> webConfig = yconf.getMap("webConfig");
