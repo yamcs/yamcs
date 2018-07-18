4eb3d8635336fca221b2904aa794208c6dd343f6
Fabian Diet <fabian.diet@spaceapplications.com>
2018-07-18 15:08:02 +0200

    Improve parameter loading somewhat

--- yamcs-web/packages/app/src/app/mdb/space-system-detail/SpaceSystemParametersTab.ts
+++ yamcs-web/packages/app/src/app/mdb/space-system-detail/SpaceSystemParametersTab.ts
@@ -17,13 +17,34 @@ export class SpaceSystemParametersTab {
   instance: Instance;
   parameters$: Promise<Parameter[]>;
 
-  constructor(route: ActivatedRoute, yamcs: YamcsService) {
+  constructor(route: ActivatedRoute, private yamcs: YamcsService) {
     const parent = route.snapshot.parent!;
     this.qualifiedName = parent.paramMap.get('qualifiedName')!;
-    this.parameters$ = yamcs.getInstanceClient()!.getParameters({
-      namespace: this.qualifiedName,
-      limit: 2500, // FIXME use proper pagination
-    });
     this.instance = yamcs.getInstance();
+    this.parameters$ = this.loadParameters();
+  }
+
+  // FIXME use proper pagination
+  private async loadParameters() {
+    const allParameters = [];
+    let page = 1;
+    const pageSize = 500;
+    while (true) {
+      const limit = pageSize + 1;
+      const pos = (page - 1) * pageSize;
+      const parameters = await this.yamcs.getInstanceClient()!.getParameters({
+        namespace: this.qualifiedName,
+        limit,
+        pos,
+      });
+      if (parameters.length === limit) {
+        allParameters.push(...parameters.slice(0, -1));
+        page += 1;
+      } else {
+        allParameters.push(...parameters);
+        break;
+      }
+    }
+    return allParameters;
   }
 }
