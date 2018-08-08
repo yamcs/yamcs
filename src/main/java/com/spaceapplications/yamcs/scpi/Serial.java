a12ec2103e3973fbfe96ef8f2b3234fb34361fad
Nick Evers <nick.evers@spaceapplications.com>
2018-08-08 11:02:40 +0200

    Move serial comms code into separate file

---
+++ src/main/java/com/spaceapplications/yamcs/scpi/Serial.java
@@ -0,0 +1,52 @@
+package com.spaceapplications.yamcs.scpi;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+
+public class Serial {
+  private void serial() {
+    printPorts();
+
+    SerialPort sp = SerialPort.getCommPort("/dev/tty.usbmodem1411");
+    sp.setBaudRate(9600);
+    sp.openPort();
+    sp.addDataListener(new SerialPortDataListener() {
+
+      @Override
+      public int getListeningEvents() {
+        return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+      }
+
+      @Override
+      public void serialEvent(SerialPortEvent event) {
+        if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+          return;
+
+        int bytes = sp.bytesAvailable();
+        System.out.print(bytes + "available: ");
+        byte[] newData = new byte[sp.bytesAvailable()];
+        sp.readBytes(newData, newData.length);
+        System.out.println(new String(newData));
+
+        
+      }
+    });
+
+    byte[] msg = "VOUT1?".getBytes();
+    sp.writeBytes(msg, msg.length);
+
+    try {
+      Thread.sleep(5000);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    sp.closePort();
+  }
+
+  private void printPorts() {
+    for (SerialPort sp : SerialPort.getCommPorts()) {
+      System.out.println(sp.getSystemPortName());
+    }
+  }
+}
