852a40f0b327d700489dca1dba2e428ee0319aa1
Nick Evers <nick.evers@spaceapplications.com>
2018-08-09 16:46:58 +0200

    Integrate config parser into main

--- src/main/java/com/spaceapplications/yamcs/scpi/Main.java
+++ src/main/java/com/spaceapplications/yamcs/scpi/Main.java
@@ -2,8 +2,6 @@ package com.spaceapplications.yamcs.scpi;
 
 import static pl.touk.throwing.ThrowingSupplier.unchecked;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 import com.spaceapplications.yamcs.scpi.config.SafeConfig;
@@ -18,26 +16,23 @@ import io.netty.handler.logging.LoggingHandler;
 
 public class Main {
   public static Integer DEFAULT_PORT = 1337;
-  public static Integer MAX_INOMING_CONNECTIONS = 5;
+  public static Integer DEFAULT_MAX_CONNECTIONS = 5;
 
   public static void main(String[] args) {
-    SafeConfig c = SafeConfig.load("config.yaml");
-    System.out.println("base: " + c.get("base"));
-    System.out.println("base.port: " + c.get("base.port"));
-    
-    Optional<List<Integer>> bla = c.get("devices.tenma");
-    
-    List<Integer> b = bla.orElse(Arrays.asList());
-    System.out.println("base.stuff: " + b);
+    new Main(Args.parse(args));
   }
 
   public Main(Args args) {
+    Optional.ofNullable(args.config).map(SafeConfig::load);
+    SafeConfig c = SafeConfig.load("config.yaml");
+    Optional<Integer> port =  c.get("daemon.port");
+
     NioEventLoopGroup bossEventLoop = new NioEventLoopGroup();
     NioEventLoopGroup workerEventLoop = new NioEventLoopGroup();
 
     try {
       ServerBootstrap b = bootstrap(bossEventLoop, workerEventLoop);
-      ChannelFuture f = unchecked(b.bind(DEFAULT_PORT)::sync).get();
+      ChannelFuture f = unchecked(b.bind(port.orElse(DEFAULT_PORT))::sync).get();
       unchecked(f.channel().closeFuture()::sync).get();
     } finally {
       bossEventLoop.shutdownGracefully();
@@ -49,7 +44,7 @@ public class Main {
     return new ServerBootstrap()
         .group(bossEventLoop, workerEventLoop)
         .channel(NioServerSocketChannel.class)
-        .option(ChannelOption.SO_BACKLOG, MAX_INOMING_CONNECTIONS)
+        .option(ChannelOption.SO_BACKLOG, DEFAULT_MAX_CONNECTIONS)
         .handler(new LoggingHandler(LogLevel.INFO))
         .childHandler(new ServerInitializer());
   }
