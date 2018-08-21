3e0a4501ae1160125ef0c718b585808cb209268a
Nick Evers <nick.evers@spaceapplications.com>
2018-08-21 12:40:42 +0200

    Wire Commander into ServerHandler

--- src/main/java/com/spaceapplications/yamcs/scpi/Commander.java
+++ src/main/java/com/spaceapplications/yamcs/scpi/Commander.java
@@ -1,9 +1,22 @@
 package com.spaceapplications.yamcs.scpi;
 
 public class Commander {
+  public static String CONFIRM = "confirm";
+
+  private static String PROMPT = "\r\n$ ";
+
   private Config config;
 
   public Commander(Config config) {
     this.config = config;
+  }
+
+  public String execute(String cmd) {
+    String msg = "error";
+    if (CONFIRM.equals(cmd))
+      msg = "Connected. Run help for more info.";
+    else
+      msg = cmd + ": command not found";
+    return msg + PROMPT;
   }
 }
--- src/main/java/com/spaceapplications/yamcs/scpi/ServerHandler.java
+++ src/main/java/com/spaceapplications/yamcs/scpi/ServerHandler.java
@@ -1,25 +1,9 @@
 package com.spaceapplications.yamcs.scpi;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Supplier;
-
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 public class ServerHandler extends SimpleChannelInboundHandler<String> {
-  private static String PROMPT = "\r\n$ ";
-
-  public interface Command extends Supplier<String> {
-  }
-
-  public static Map<String, Command> commands = new HashMap<>();
-
-  {
-    commands.put("help", () -> "help here");
-    commands.put("list", () -> "list devices here");
-  }
-
   private Commander commander;
 
   public ServerHandler(Commander commander) {
@@ -28,14 +12,14 @@ public class ServerHandler extends SimpleChannelInboundHandler<String> {
 
   @Override
   public void channelActive(ChannelHandlerContext ctx) throws Exception {
-    ctx.write("Connected. Run help for more info." + PROMPT);
+    ctx.write(commander.execute(Commander.CONFIRM));
     ctx.flush();
   }
 
   @Override
-  protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-    Command c = commands.getOrDefault(msg, () -> msg + ": command not found");
-    ctx.write(c.get() + PROMPT);
+  protected void channelRead0(ChannelHandlerContext ctx, String cmd) throws Exception {
+    ctx.write(commander.execute(cmd));
+    ctx.flush();
   }
 
   @Override
