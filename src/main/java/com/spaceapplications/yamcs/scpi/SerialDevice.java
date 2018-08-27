29f3c6d5cd5e1c1fde16a2d1665687a336e8be9c
Nick Evers <nick.evers@spaceapplications.com>
2018-08-27 15:15:42 +0200

    Implement SerialDevice

--- src/main/java/com/spaceapplications/yamcs/scpi/SerialDevice.java
+++ src/main/java/com/spaceapplications/yamcs/scpi/SerialDevice.java
@@ -1,59 +1,45 @@
 package com.spaceapplications.yamcs.scpi;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import com.fazecast.jSerialComm.SerialPort;
-import com.fazecast.jSerialComm.SerialPortDataListener;
-import com.fazecast.jSerialComm.SerialPortEvent;
 import com.spaceapplications.yamcs.scpi.commander.DeviceConnect.Device;
 
 public class SerialDevice implements Device {
   private String id;
   private String devicePath;
-  private SerialPort sp;
-
-
-  @Override
-  public void open() {
-    SerialPort sp = SerialPort.getCommPort(devicePath);
-    sp.setBaudRate(9600);
-    sp.openPort();
-
-    sp.addDataListener(new SerialPortDataListener() {
-      @Override
-      public int getListeningEvents() {
-        return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
-      }
-
-      @Override
-      public void serialEvent(SerialPortEvent event) {
-        if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
-          return;
-
-        int bytes = sp.bytesAvailable();
-        System.out.print(bytes + "available: ");
-        byte[] newData = new byte[sp.bytesAvailable()];
-        sp.readBytes(newData, newData.length);
-        System.out.println(new String(newData));
-      }
-    });
-
-    byte[] msg = "VOUT1?".getBytes();
-    sp.writeBytes(msg, msg.length);
-
-  }
+  private static SerialPort sp;
 
   public SerialDevice(String id, String devicePath) {
     this.id = id;
     this.devicePath = devicePath;
   }
 
-  private void serial() {
-
-    try {
-      Thread.sleep(5000);
-    } catch (Exception e) {
-      e.printStackTrace();
+  @Override
+  public void open() {
+    if (sp == null) {
+      sp = SerialPort.getCommPort(devicePath);
+      sp.setBaudRate(9600);
+      sp.openPort();
+      sp.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 100, 0);
     }
-    sp.closePort();
+  }
+
+  @Override
+  public String exec(String cmd) {
+    byte[] bytes = cmd.getBytes();
+    sp.writeBytes(bytes, bytes.length);
+    ByteArrayOutputStream buf = new ByteArrayOutputStream();
+    byte[] singleByte = { 0 };
+    while (sp.readBytes(singleByte, 1) > 0) {
+      try {
+        buf.write(singleByte);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return buf.toString();
   }
 
   @Override
@@ -61,14 +47,7 @@ public class SerialDevice implements Device {
     return id;
   }
 
-
   @Override
   public void close() {
-    sp.removeDataListener();
-  }
-
-  @Override
-  public String exec(String cmd) {
-    return "exec: " + cmd + " on " + id;
   }
 }
