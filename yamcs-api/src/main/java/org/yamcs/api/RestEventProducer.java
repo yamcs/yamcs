34b2cd7f079bbc8dbb84e56d3239d6cf5bead886
Fabian Diet <fabian.diet@spaceapplications.com>
2018-08-28 19:50:05 +0200

    Use new event api in RestEventProducer

--- yamcs-api/src/main/java/org/yamcs/api/RestEventProducer.java
+++ yamcs-api/src/main/java/org/yamcs/api/RestEventProducer.java
@@ -8,48 +8,47 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yamcs.ConfigurationException;
 import org.yamcs.api.rest.RestClient;
+import org.yamcs.protobuf.Rest.CreateEventRequest;
 import org.yamcs.protobuf.Yamcs.Event;
 import org.yamcs.utils.TimeEncoding;
 import org.yaml.snakeyaml.Yaml;
 
 import io.netty.handler.codec.http.HttpMethod;
 
-
 /**
  * An EventProducer that publishes events over WebSocket
  * <p>
- * By default, repeated message are detected and reduced, resulting in pseudo
- * events with a message like 'last event repeated X times'. This behaviour can
- * be turned off.
+ * By default, repeated message are detected and reduced, resulting in pseudo events with a message like 'last event
+ * repeated X times'. This behaviour can be turned off.
  */
 public class RestEventProducer extends AbstractEventProducer {
     static final String CONF_REPEATED_EVENT_REDUCTION = "repeatedEventReduction";
-    
+
     RestClient restClient;
-    static Logger logger=LoggerFactory.getLogger(RestEventProducer.class);
-    
-    static final int MAX_QUEUE_SIZE=1000;
-    ArrayBlockingQueue<Event> queue=new ArrayBlockingQueue<Event>(MAX_QUEUE_SIZE);
-    
+    static Logger logger = LoggerFactory.getLogger(RestEventProducer.class);
+
+    static final int MAX_QUEUE_SIZE = 1000;
+    ArrayBlockingQueue<Event> queue = new ArrayBlockingQueue<>(MAX_QUEUE_SIZE);
+
     String eventResource;
     YamcsConnectionProperties connProp;
-    
+
     public RestEventProducer(YamcsConnectionProperties connProp) {
         restClient = new RestClient(connProp);
         this.connProp = connProp;
-        
-        eventResource = "/archive/"+connProp.getInstance()+"/events";
+
+        eventResource = "/archive/" + connProp.getInstance() + "/events";
         InputStream is = RestEventProducer.class.getResourceAsStream("/event-producer.yaml");
         boolean repeatedEventReduction = true;
-        if(is!=null) {
+        if (is != null) {
             Object o = new Yaml().load(is);
-            if(!(o instanceof Map<?,?>)) {
-                throw new ConfigurationException("event-producer.yaml does not contain a map but a "+o.getClass());
+            if (!(o instanceof Map<?, ?>)) {
+                throw new ConfigurationException("event-producer.yaml does not contain a map but a " + o.getClass());
             }
-    
+
             @SuppressWarnings("unchecked")
-            Map<String,Object> m = (Map<String, Object>) o;
-    
+            Map<String, Object> m = (Map<String, Object>) o;
+
             if (m.containsKey(CONF_REPEATED_EVENT_REDUCTION)) {
                 repeatedEventReduction = (Boolean) m.get(CONF_REPEATED_EVENT_REDUCTION);
             }
@@ -58,38 +57,46 @@ public class RestEventProducer extends AbstractEventProducer {
             setRepeatedEventReduction(true, 60000);
         }
     }
-    
-    
-
 
     @Override
     public void close() {
         restClient.close();
     }
-    
-    
+
     /* (non-Javadoc)
      * @see org.yamcs.api.EventProducer#sendEvent(org.yamcs.protobuf.Yamcs.Event)
      */
     @Override
     public synchronized void sendEvent(Event event) {
         logger.debug("Sending Event: {}", event.getMessage());
-        restClient.doRequest(eventResource, HttpMethod.POST, event.toByteArray());
+        CreateEventRequest.Builder req = CreateEventRequest.newBuilder();
+        req.setMessage(event.getMessage());
+        if (event.hasGenerationTimeUTC()) {
+            req.setTime(event.getGenerationTimeUTC());
+        } else if (event.hasGenerationTime()) {
+            req.setTime(TimeEncoding.toString(event.getGenerationTime()));
+        }
+        if (event.hasSeverity()) {
+            req.setSeverity(event.getSeverity().toString());
+        }
+        if (event.hasType()) {
+            req.setType(event.getType());
+        }
+        restClient.doRequest(eventResource, HttpMethod.POST, req.build().toByteArray());
     }
-    
+
     @Override
     public String toString() {
-        return RestEventProducer.class.getName()+" sendign events to "+connProp;
+        return RestEventProducer.class.getName() + " sendign events to " + connProp;
     }
-    
+
     @Override
-    public long getMissionTime() {       
+    public long getMissionTime() {
         return TimeEncoding.getWallclockTime();
     }
-    
-    
+
     public void main(String[] args) {
-        
+
     }
 
 }
