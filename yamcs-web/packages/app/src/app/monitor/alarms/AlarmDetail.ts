ff25ef5dfb99ee062180fb03340e0398900266d2
Fabian Diet <fabian.diet@spaceapplications.com>
2018-09-23 13:49:17 +0200

    Improve alarm detail a bit

--- yamcs-web/packages/app/src/app/monitor/alarms/AlarmDetail.ts
+++ yamcs-web/packages/app/src/app/monitor/alarms/AlarmDetail.ts
@@ -1,17 +1,48 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
-import { Alarm } from '@yamcs/client';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnDestroy } from '@angular/core';
+import { Alarm, Instance } from '@yamcs/client';
+import { BehaviorSubject } from 'rxjs';
+import { YamcsService } from '../../core/services/YamcsService';
 import { DyDataSource } from '../../shared/widgets/DyDataSource';
 
 @Component({
   selector: 'app-alarm-detail',
   templateUrl: './AlarmDetail.html',
+  styleUrls: ['./AlarmDetail.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AlarmDetail {
+export class AlarmDetail implements OnChanges, OnDestroy {
 
   @Input()
   alarm: Alarm;
 
   @Input()
-  plotDataSource: DyDataSource;
+  instance: Instance;
+
+  plotDataSource$ = new BehaviorSubject<DyDataSource | null>(null);
+
+  constructor(private yamcs: YamcsService) {
+  }
+
+  ngOnChanges() {
+    this.disconnectCurrentDataSource();
+
+    const plotDataSource = new DyDataSource(this.yamcs);
+    plotDataSource.addParameter(this.alarm.parameter);
+
+    // TODO disabled because plot does not follow selection yet.
+    // probably because ParameterPlot should support OnChanges
+    // this.plotDataSource$.next(plotDataSource);
+  }
+
+  ngOnDestroy() {
+    this.disconnectCurrentDataSource();
+    this.plotDataSource$.unsubscribe();
+  }
+
+  private disconnectCurrentDataSource() {
+    const dataSource = this.plotDataSource$.getValue();
+    if (dataSource) {
+      dataSource.disconnect();
+    }
+  }
 }
