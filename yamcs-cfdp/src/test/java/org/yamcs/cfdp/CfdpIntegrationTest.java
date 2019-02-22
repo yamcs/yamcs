fedd2b894c8285df72e0b7ebbbd78edb7c268ffc
Nicolae Mihalache <nicolae.mihalache@spaceapplications.com>
2019-02-22 13:24:04 +0100

    added some proto definitions for the cfdp transfers

--- yamcs-cfdp/src/test/java/org/yamcs/cfdp/CfdpIntegrationTest.java
+++ yamcs-cfdp/src/test/java/org/yamcs/cfdp/CfdpIntegrationTest.java
@@ -3,6 +3,7 @@ package org.yamcs.cfdp;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Random;
 import java.util.concurrent.Future;
@@ -15,12 +16,16 @@ import org.yamcs.YamcsServer;
 import org.yamcs.api.MediaType;
 import org.yamcs.api.YamcsConnectionProperties;
 import org.yamcs.api.rest.RestClient;
+import org.yamcs.protobuf.Cfdp.TransferStatus;
 import org.yamcs.utils.FileUtils;
 import org.yamcs.yarch.Bucket;
 import org.yamcs.yarch.BucketDatabase;
 import org.yamcs.yarch.Stream;
 import org.yamcs.yarch.YarchDatabase;
 import org.yamcs.yarch.YarchDatabaseInstance;
+
+import com.google.protobuf.Message;
+import com.google.protobuf.util.JsonFormat;
 
 import io.netty.handler.codec.http.HttpMethod;
 
@@ -60,12 +65,25 @@ public class CfdpIntegrationTest {
     private void uploadAndCheck(String objName, byte[] data) throws Exception {
         Future<String> responseFuture = restClient.doRequest("/cfdp/"+yamcsInstance+"/"+bucketName+"/"+objName+"?target=cfdp-tgt1", HttpMethod.POST, "");
         
-       
-        responseFuture.get();//TODO check response
+        TransferStatus ts = fromJson(responseFuture.get(), TransferStatus.newBuilder()).build();
+        int id = 5;
         MyFileReceiver rx = new MyFileReceiver();
         
-        //TODO ...check somehow that the file has finished transfer
+
+        //TODO ...check maybe in a loop that the transfer is progressing until finished
+        
+        while(ts.getSizeTransferred()<ts.getTotalSize()) {
+            responseFuture = restClient.doRequest("/cfdp/"+yamcsInstance+"/info?id="+id, HttpMethod.GET, "");
+            ts = fromJson(responseFuture.get(), TransferStatus.newBuilder()).build();
+            //TODO assert this assert that
+            Thread.sleep(1000);
+        } 
+       
+        
+
         assertEquals(data, rx.data);
+        
+        //more checks
         
     }
     
@@ -100,4 +118,11 @@ public class CfdpIntegrationTest {
     protected static String getYamcsInstance() {
         return "cfdp";
     }
+    
+    
+    <T extends Message.Builder> T fromJson(String json, T builder) throws IOException {
+        JsonFormat.parser().merge(json, builder);
+        return builder;
+    }
+
 }
