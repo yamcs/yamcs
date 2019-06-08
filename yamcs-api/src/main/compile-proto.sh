699c6a487155149ebc0688d683c8ae4f958ad8ad
Fabian Diet <fabian.diet@spaceapplications.com>
2019-06-08 14:37:04 +0200

    Let Maven generate proto java sources

--- yamcs-api/src/main/compile-proto.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-protoc --proto_path=proto --java_out=java  `find . -name '*.proto'`
--- yamcs-core/src/main/compile-proto.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-protoc --java_out=java tablespace.proto
-protoc --proto_path=../../../yamcs-api/src/main/proto --proto_path=. --java_out=java cmdhistory.proto
