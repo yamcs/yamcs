35cbecc5a9cba112fd506906437b028952a9adef
Fabian Diet <fabian.diet@spaceapplications.com>
2019-06-21 11:33:12 +0200

    Remove BogusSAT-1.xml symlink for maven reasons
    Recent versions of maven-resources-plugin
    seem to have trouble with symlinks when not doing a clean

--- yamcs-xtce/src/test/java/org/yamcs/xtce/TestParameterTypeConstructors.java
+++ yamcs-xtce/src/test/java/org/yamcs/xtce/TestParameterTypeConstructors.java
@@ -1,7 +1,8 @@
 package org.yamcs.xtce;
 
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
