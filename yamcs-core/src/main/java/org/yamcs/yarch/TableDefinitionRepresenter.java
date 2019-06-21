d0fb4d5739937b86582e54ddda291c05599717e3
Fabian Diet <fabian.diet@spaceapplications.com>
2019-06-21 11:26:28 +0200

    Upgrade snakeyaml dependency

--- yamcs-core/src/main/java/org/yamcs/yarch/TableDefinitionRepresenter.java
+++ yamcs-core/src/main/java/org/yamcs/yarch/TableDefinitionRepresenter.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.yamcs.yarch.PartitioningSpec._type;
 import org.yamcs.yarch.TableDefinition.PartitionStorage;
+import org.yaml.snakeyaml.DumperOptions.FlowStyle;
 import org.yaml.snakeyaml.nodes.Node;
 import org.yaml.snakeyaml.nodes.Tag;
 import org.yaml.snakeyaml.representer.Represent;
@@ -14,6 +15,7 @@ import org.yaml.snakeyaml.representer.Representer;
 
 /**
  * Serializes TableDefinition to yaml format
+ * 
  * @author nm
  *
  */
@@ -24,14 +26,13 @@ public class TableDefinitionRepresenter extends Representer {
     public static final String K_DATA_DIR = "dataDir";
     public static final String K_HISTOGRAM = "histogram";
     public static final String K_ENUM_VALUE = "enumValues";
-    public static final String K_PARTITIONING_SPEC = "partitioningSpec";	
+    public static final String K_PARTITIONING_SPEC = "partitioningSpec";
     public static final String K_TIME_COLUMN = "timeColumn";
     public static final String K_VALUE_COLUMN = "valueColumn";
     public static final String K_TIME_PARTITIONING_SCHEMA = "timePartitioningSchema";
     public static final String K_STORAGE_ENGINE = "storageEngine";
     public static final String K_PARTITION_STORAGE = "partitionStorage";
     public static final String K_FORMAT_VERSION = "formatVersion";
-
 
     public TableDefinitionRepresenter() {
         this.representers.put(TableDefinition.class, new RepresentTableDefinition());
@@ -44,29 +45,29 @@ public class TableDefinitionRepresenter extends Representer {
         @Override
         public Node representData(Object data) {
             TableDefinition td = (TableDefinition) data;
-            Map<String, Object> m=new HashMap<>();
+            Map<String, Object> m = new HashMap<>();
             m.put(K_COMPRESSED, td.isCompressed());
             m.put(K_KEY_DEF, td.getKeyDefinition());
             m.put(K_VALUE_DEF, td.serializedValueDef);
             m.put(K_STORAGE_ENGINE, td.getStorageEngineName());
-            if(td.getPartitionStorage()!=null) {
+            if (td.getPartitionStorage() != null) {
                 m.put(K_PARTITION_STORAGE, td.getPartitionStorage());
             }
             m.put(K_FORMAT_VERSION, td.getFormatVersion());
 
-            if(td.hasHistogram()) {
+            if (td.hasHistogram()) {
                 m.put(K_HISTOGRAM, td.getHistogramColumns());
             }
-            if(td.serializedEmumValues!=null) {
+            if (td.serializedEmumValues != null) {
                 m.put(K_ENUM_VALUE, td.serializedEmumValues);
             }
-            if(td.hasCustomDataDir()) {
+            if (td.hasCustomDataDir()) {
                 m.put(K_DATA_DIR, td.getDataDir());
             }
-            if(td.hasPartitioning()) {
+            if (td.hasPartitioning()) {
                 m.put(K_PARTITIONING_SPEC, td.getPartitioningSpec());
             }
-            return representMapping(new Tag("TableDefinition"), m, false);
+            return representMapping(new Tag("TableDefinition"), m, FlowStyle.BLOCK);
         }
     }
 
@@ -74,16 +75,16 @@ public class TableDefinitionRepresenter extends Representer {
         @Override
         public Node representData(Object data) {
             TupleDefinition td = (TupleDefinition) data;
-            List<Object> list=new ArrayList<>(td.size());
-            for(int i=0;i<td.size();i++) {
-                ColumnDefinition cd=td.getColumn(i);
-                Map<String, Object> m=new HashMap<>();
+            List<Object> list = new ArrayList<>(td.size());
+            for (int i = 0; i < td.size(); i++) {
+                ColumnDefinition cd = td.getColumn(i);
+                Map<String, Object> m = new HashMap<>();
                 m.put("idx", i);
                 m.put("name", cd.getName());
                 m.put("type", cd.getType().toString());
                 list.add(m);
             }
-            return representSequence(new Tag("TupleDefinition"), list, false);
+            return representSequence(new Tag("TupleDefinition"), list, FlowStyle.BLOCK);
         }
     }
 
@@ -91,18 +92,17 @@ public class TableDefinitionRepresenter extends Representer {
         @Override
         public Node representData(Object data) {
             PartitioningSpec p = (PartitioningSpec) data;
-            Map<String, Object> m=new HashMap<>();
+            Map<String, Object> m = new HashMap<>();
             m.put("type", p.type.toString());
-            if((p.type==_type.TIME) || (p.type==_type.TIME_AND_VALUE)) {
+            if ((p.type == _type.TIME) || (p.type == _type.TIME_AND_VALUE)) {
                 m.put("timeColumn", p.timeColumn);
                 m.put(K_TIME_PARTITIONING_SCHEMA, p.getTimePartitioningSchema().getName());
             }
-            if((p.type==_type.VALUE) || (p.type==_type.TIME_AND_VALUE)) {
+            if ((p.type == _type.VALUE) || (p.type == _type.TIME_AND_VALUE)) {
                 m.put(K_VALUE_COLUMN, p.valueColumn);
             }
 
-
-            return representMapping(new Tag("PartitioningSpec"), m, true);
+            return representMapping(new Tag("PartitioningSpec"), m, FlowStyle.FLOW);
         }
     }
 
