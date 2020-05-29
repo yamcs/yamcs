c863b41c61b600c609e187c027d31db2f0a59f7e
Nicolae Mihalache <xpromache@gmail.com>
2020-05-29 12:25:39 +0200

    fix to work on Linux

--- run-example.sh
+++ run-example.sh
@@ -2,20 +2,22 @@
 
 YAMCS_OPTS="$YAMCS_OPTS ${@:2}"
 
+print_example_list () {
+    for dir in `find examples -maxdepth 1 -mindepth 1 -type d ! -name '.*' -exec basename {} \; | sort`; do
+        echo "    $dir"
+    done
+}
+
 if [[ -z "$1" ]]; then
     echo "usage: $0 EXAMPLE [options]"
     echo
     echo "Where EXAMPLE is one of:"
-    for dir in `find examples -type d -depth 1 -exec basename {} \; | sort`; do
-        echo "    $dir"
-    done
+    print_example_list
     exit 1
 fi
 if [[ ! -d "examples/$1" ]]; then
     echo "Cannot find an example by the name '$1'. Use one of:"
-    for dir in `find examples -type d -depth 1 -exec basename {} \; | sort`; do
-        echo "    $dir"
-    done
+    print_example_list
     exit 1
 fi
 
