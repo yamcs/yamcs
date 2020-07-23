ad9407d160e7fa1a3a1a6637783e7fc487d6a8c3
Nicolae Mihalache <xpromache@gmail.com>
2020-07-23 07:06:42 +0200

    forgotten file

---
+++ yamcs-core/src/main/java/org/yamcs/cmdhistory/Attribute.java
@@ -0,0 +1,21 @@
+package org.yamcs.cmdhistory;
+
+import org.yamcs.parameter.Value;
+
+public class Attribute {
+    final String key;
+    final Value value;
+
+    public Attribute(String key, Value value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+}
