2e6fcbaabd8da5f07585ec8a0c9662138d4a2583
Nicolae Mihalache <xpromache@gmail.com>
2020-10-29 12:39:40 +0100

    ccsds-frames example stores the frames with the high resolution ert

--- examples/ccsds-frames/src/main/yamcs/etc/extra_streams.sql
+++ examples/ccsds-frames/src/main/yamcs/etc/extra_streams.sql
@@ -4,10 +4,10 @@ insert into invalid_tm select * from invalid_tm_stream
 
 -- create the good and bad frame streams and tables. The definition has to match the one from org.yamcs.tctm.ccsds.FrameStreamHelper
 
-create stream good_frame_stream(rectime timestamp, seq int, ertime timestamp, scid int, vcid int, data binary)
-create table if not exists good_frames(rectime timestamp, seq int, ertime timestamp, scid int, vcid int, data binary, primary key(rectime, seq))
+create stream good_frame_stream(rectime timestamp, seq int, ertime hres_timestamp, scid int, vcid int, data binary)
+create table if not exists good_frames(rectime timestamp, seq int, ertime hres_timestamp, scid int, vcid int, data binary, primary key(rectime, seq))
 insert into good_frames select * from good_frame_stream
 
-create stream bad_frame_stream(rectime timestamp, seq int, ertime timestamp, data binary)
-create table if not exists bad_frames(rectime timestamp, seq int, ertime timestamp, data binary, primary key(rectime, seq))
+create stream bad_frame_stream(rectime timestamp, seq int, ertime hres_timestamp, data binary)
+create table if not exists bad_frames(rectime timestamp, seq int, ertime hres_timestamp, data binary, primary key(rectime, seq))
 insert into bad_frames select * from bad_frame_stream
