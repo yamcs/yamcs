6f620edf5dcb07db958aab1bd758415c008f93dc
Fabian Diet <fabian.diet@spaceapplications.com>
2021-03-06 22:26:22 +0100

    Support setting binary and aggregate parameters

---
+++ yamcs-web/src/main/webapp/src/app/telemetry/parameters/ParameterForm.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { Parameter } from '../../client';
+
+@Component({
+  selector: 'app-parameter-form',
+  templateUrl: './ParameterForm.html',
+  styleUrls: ['./ParameterForm.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ParameterForm implements OnInit {
+
+  @Input()
+  formGroup: FormGroup;
+
+  @Input()
+  parameter: Parameter;
+
+  @Input()
+  parent: string;
+
+  controlName$ = new BehaviorSubject<string | null>(null);
+
+  ngOnInit() {
+    if (this.parent) {
+      this.controlName$.next(this.parent + '.' + this.parameter.name);
+    } else {
+      this.controlName$.next(this.parameter.name);
+    }
+  }
+}
