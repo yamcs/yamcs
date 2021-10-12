acbec97edec487e17d5671778153f7b8106d26e6
Nicolae Mihalache <xpromache@gmail.com>
2021-10-12 04:50:14 +0200

    RealtimeFiller fixes
    - refactored the RealtimeFiller.SegmentQueue into a static class to allow unit tests
    - fixed the sortingThreshold interpretation
    - fixed the SegmentIterator iteration order (when ascending, the realtime filler should be last)
    
    See #618 and #619

--- yamcs-core/src/main/java/org/yamcs/parameterarchive/RealtimeArchiveFiller.java
+++ yamcs-core/src/main/java/org/yamcs/parameterarchive/RealtimeArchiveFiller.java
@@ -8,9 +8,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import org.rocksdb.RocksDBException;
 import org.yamcs.ConfigurationException;
@@ -123,7 +125,7 @@ public class RealtimeArchiveFiller extends AbstractArchiveFiller {
 
         log.info("Shutting down, writing all pending segments");
         for (SegmentQueue queue : queues.values()) {
-            queue.flush();
+            queue.flush(pgs -> scheduleWriteToArchive(pgs));
         }
         executor.shutdown();
         if (!executor.awaitTermination(30, TimeUnit.SECONDS)) {
@@ -133,17 +135,56 @@ public class RealtimeArchiveFiller extends AbstractArchiveFiller {
 
     @Override
     protected void processParameters(long t, BasicParameterList pvList) {
+        int parameterGroupId;
         try {
-            int parameterGroupId = parameterGroupIdMap.createAndGet(pvList.getPids());
-
-            SegmentQueue segQueue = queues.computeIfAbsent(parameterGroupId,
-                    id -> new SegmentQueue(parameterGroupId, pvList.getPids()));
-            segQueue.addRecord(t, pvList.getValues());
+            parameterGroupId = parameterGroupIdMap.createAndGet(pvList.getPids());
         } catch (RocksDBException e) {
-            log.error("Error writing to the parameter archive", e);
-        } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Error creating parameter group id", e);
+            return;
         }
+
+        SegmentQueue segQueue = queues.computeIfAbsent(parameterGroupId,
+                id -> new SegmentQueue(parameterGroupId, pvList.getPids(), maxSegmentSize));
+
+        synchronized (segQueue) {
+            if (!segQueue.isEmpty()) {
+                long segStart = segQueue.getStart();
+                if (t < segStart - pastJumpThreshold) {
+                    log.warn(
+                            "Time jumped in the past; current timestamp: {}, new timestamp: {}. Flushing old data.",
+                            TimeEncoding.toString(segStart), TimeEncoding.toString(t));
+                    segQueue.flush(pgs -> scheduleWriteToArchive(pgs));
+                } else if (t < segQueue.getStart() - sortingThreshold) {
+                    log.warn("Dropping old data with timestamp {} (minimum allowed is {})."
+                            + "Unsorted data received in the realtime filler? Consider using a backfiller instead",
+                            TimeEncoding.toString(t),
+                            TimeEncoding.toString(segStart - sortingThreshold));
+                    return;
+                } else {
+                    segQueue.sendToArchive(segStart - sortingThreshold, pgs -> scheduleWriteToArchive(pgs));
+                }
+            }
+
+            if (!segQueue.addRecord(t, pvList.getValues())) {
+                log.warn("Realtime parameter archive queue full."
+                        + "Consider increasing the writerThreads (if CPUs are available) or using a back filler");
+            }
+        }
+
+    }
+
+    private CompletableFuture<Void> scheduleWriteToArchive(PGSegment pgs) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                long t0 = System.nanoTime();
+                parameterArchive.writeToArchive(pgs);
+                long d = System.nanoTime() - t0;
+                log.debug("Wrote segment {} to archive in {} millisec", pgs, d / 1000_000);
+            } catch (RocksDBException | IOException e) {
+                log.error("Error writing segment to the parameter archive", e);
+            }
+            return null;
+        }, executor);
     }
 
     @Override
@@ -207,7 +248,7 @@ public class RealtimeArchiveFiller extends AbstractArchiveFiller {
      * 
      *
      */
-    class SegmentQueue {
+    static class SegmentQueue {
         static final int QSIZE = 16; // has to be a power of 2!
         static final int MASK = QSIZE - 1;
         final PGSegment[] segments = new PGSegment[QSIZE];
@@ -216,30 +257,22 @@ public class RealtimeArchiveFiller extends AbstractArchiveFiller {
 
         final int parameterGroupId;
         final IntArray parameterIds;
+        final int maxSegmentSize;
 
-        public SegmentQueue(int parameterGroupId, IntArray parameterIds) {
+        public SegmentQueue(int parameterGroupId, IntArray parameterIds, int maxSegmentSize) {
             this.parameterGroupId = parameterGroupId;
             this.parameterIds = parameterIds;
+            this.maxSegmentSize = maxSegmentSize;
         }
 
-        public synchronized void addRecord(long t, List<BasicParameterValue> values) {
-            if (head != tail) {
-                PGSegment segHead = segments[head];
-
-                if (t < segHead.getSegmentStart() - pastJumpThreshold) {
-                    log.warn("Time jumped in the past; current timestamp: {}, new timestamp: {}. Flushing old data.",
-                            TimeEncoding.toString(segHead.getSegmentStart()), TimeEncoding.toString(t));
-                    flush();
-                } else if (t < segHead.getSegmentStart() - sortingThreshold) {
-                    log.warn("Dropping old data with timestamp {} (minimum allowed is {})."
-                            + "Unsorted data received in the realtime filler? Consider using a backfiller instead",
-                            TimeEncoding.toString(t),
-                            TimeEncoding.toString(segHead.getSegmentStart() - sortingThreshold));
-                    return;
-                } else {
-                    writeToArchive(t);
-                }
+        public long getStart() {
+            if (isEmpty()) {
+                throw new IllegalStateException("queue is empty");
             }
+            return segments[head].getSegmentStart();
+        }
+
+        public synchronized boolean addRecord(long t, List<BasicParameterValue> values) {
 
             boolean added = false;
             int k = head;
@@ -253,35 +286,23 @@ public class RealtimeArchiveFiller extends AbstractArchiveFiller {
                 } else if (kintv > tintv) {
                     break;
                 }
-                assert (t >= seg.getSegmentStart());
 
                 if (t <= seg.getSegmentEnd() || seg.size() < maxSegmentSize) {
-                    // when the first condition is met only, the segment will become bigger than the maxSegmentSize
-                    // could be split in two
+                    // when the first condition is met only (i.e. new data coming in the middle of a full segment)
+                    // the segment will become bigger than the maxSegmentSize
+                    // FIXME: split the interval in two if this happens
                     seg.addRecord(t, values);
                     added = true;
                     break;
                 }
             }
 
-            if (!added) {// to be added on position k
+            if (!added) {// new segment to be added on position k
                 if (segments[tail] != null) {
-                    log.warn("Realtime parameter archive queue full."
-                            + "Consider increasing the writerThreads (if CPUs are available) or using a back filler");
-                    return;
+                    return false;
                 }
 
-                long segStart = t - sortingThreshold;
-                PGSegment prev = segments[k - 1 & MASK];
-                if (prev != null) {
-                    if (segStart <= prev.getSegmentEnd()) {
-                        segStart = prev.getSegmentEnd() + 1;
-                    }
-                }
-                if (segStart < tintv) {
-                    segStart = tintv;
-                }
-                PGSegment seg = new PGSegment(parameterGroupId, segStart, parameterIds);
+                PGSegment seg = new PGSegment(parameterGroupId, t, parameterIds);
                 seg.addRecord(t, values);
 
                 // shift everything between k and tail to the right
@@ -294,18 +315,18 @@ public class RealtimeArchiveFiller extends AbstractArchiveFiller {
                 segments[k] = seg;
 
             }
+            return true;
         }
 
         /**
-         * write to archive all segments that cannot get new data.
+         * send to archive all segments which are either from an older interval than t1 or are full and their end is
+         * smaller than t1.
          * <p>
-         * t is the timestamp of the newest received data, we won't accept data older than t1=t-sortingThrreshold
-         * <p>
-         * it means that all segments which are either from an older interval than t1 or are full and their end is
-         * smaller than t1, can be written
+         * Writing to archive is an async operation, and the completable future returned by the function is called when
+         * the writing to archive has been completed and is used to null the entry in the queue. Before the entry is
+         * null, the data can still be used in the retrieval.
          */
-        private void writeToArchive(long t) {
-            long t1 = t - sortingThreshold;
+        void sendToArchive(long t1, Function<PGSegment, CompletableFuture<Void>> f) {
             while (head != tail) {
                 PGSegment seg = segments[head];
 
@@ -315,28 +336,21 @@ public class RealtimeArchiveFiller extends AbstractArchiveFiller {
                 }
                 int _head = head;
                 head = (head + 1) & MASK;
-                sendToExecutor(_head);
+                toArchive(_head, f);
             }
         }
 
-        public synchronized void flush() {
+        synchronized void flush(Function<PGSegment, CompletableFuture<Void>> f) {
             while (head != tail) {
-                sendToExecutor(head);
+                toArchive(head, f);
                 head = (head + 1) & MASK;
             }
         }
 
-        private void sendToExecutor(int idx) {
-            executor.submit(() -> {
-                try {
-                    PGSegment seg = segments[idx];
-                    long t0 = System.nanoTime();
-                    parameterArchive.writeToArchive(seg);
-                    long d = System.nanoTime() - t0;
-                    log.debug("Wrote segment {} to archive in {} millisec", seg, d / 1000_000);
-                } catch (RocksDBException | IOException e) {
-                    log.error("Error writing segment to the parameter archive", e);
-                }
+        private void toArchive(int idx, Function<PGSegment, CompletableFuture<Void>> f) {
+            PGSegment seg = segments[idx];
+
+            f.apply(seg).thenAccept(v -> {
                 segments[idx] = null;
             });
         }
@@ -349,6 +363,14 @@ public class RealtimeArchiveFiller extends AbstractArchiveFiller {
             return head == tail;
         }
 
+        /**
+         * Returns a list of segments for the pid.
+         * 
+         * <p>
+         * The ascending argument can be used to sort the segments in ascending or descending order. The values inside
+         * the segments will always be ascending (but one can iterate the segment in descending order).
+         * 
+         */
         public synchronized List<ParameterValueSegment> getPVSegments(int pid, boolean ascending) {
             if (head == tail) {
                 return Collections.emptyList();
