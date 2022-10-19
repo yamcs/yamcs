1d5c39dc8676e0e126bb77b6d620c0473bebc5a4
Fabian Diet <fabian.diet@spaceapplications.com>
2022-10-19 19:35:48 +0200

    Reduce output when serialized MDB became incompatible

--- yamcs-core/src/main/java/org/yamcs/mdb/XtceDbFactory.java
+++ yamcs-core/src/main/java/org/yamcs/mdb/XtceDbFactory.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InvalidClassException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
@@ -131,8 +132,11 @@ public class XtceDbFactory {
             try {
                 db = loadSerializedInstance(serializedFile);
                 serializedLoaded = true;
+            } catch (InvalidClassException e) {
+                log.debug("Cannot load serialized database: " + e.getMessage());
+                db = null;
             } catch (Exception e) {
-                log.info("Cannot load serialized database", e);
+                log.warn("Cannot load serialized database", e);
                 db = null;
             }
         }
