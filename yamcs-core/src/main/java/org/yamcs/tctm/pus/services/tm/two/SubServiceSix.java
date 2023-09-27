97b8003371395a5aa345ae6ac9a6db5100b14d38
fubaa <swarup@pixxel.co.in>
2023-09-27 15:35:08 +0530

    GSS-208: Add support for Service-2:SubService-9 | Physical device data report

--- yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tm/two/SubServiceSix.java
+++ yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tm/two/SubServiceSix.java
@@ -2,46 +2,47 @@ package org.yamcs.tctm.pus.services.tm.two;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.yamcs.InitException;
+import org.yamcs.TmPacket;
 import org.yamcs.YConfiguration;
 import org.yamcs.yarch.Bucket;
-import org.yamcs.filetransfer.FileSaveHandler;
 import org.yamcs.logging.Log;
 import org.yamcs.tctm.pus.services.PusSubService;
+import org.yamcs.tctm.pus.services.tm.BucketSaveHandler;
 import org.yamcs.tctm.pus.services.tm.PusTmPacket;
 import org.yamcs.utils.ByteArrayUtils;
-import org.yamcs.yarch.YarchDatabase;
-import org.yamcs.yarch.YarchDatabaseInstance;
 import org.yamcs.yarch.YarchException;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class SubServiceSix implements PusSubService {
+public class SubServiceSix extends BucketSaveHandler implements PusSubService {
     String yamcsInstance;
     Log log;
 
-    int registerAddressSize;
-    int registerValueSize;
-    int registerStartOffset;
+    private static int REGISTER_ADDRESS_SIZE = 4;
+    private static int REGISTER_VALUE_SIZE = 4;
+
+    private static int registerAddressSize;
+    private static int registerValueSize;
+    private static int registerStartOffset;
 
     Bucket registerDumpBucket;
-    FileSaveHandler fileSaveHandler;
-
     Gson gson;
 
     public SubServiceSix(String yamcsInstance, YConfiguration subServiceSixConfig) {
         this.yamcsInstance = yamcsInstance;
         log = new Log(getClass(), yamcsInstance);
 
-        registerAddressSize = subServiceSixConfig.getInt("addressSize", 4);
-        registerValueSize = subServiceSixConfig.getInt("valueSize", 4);
+        registerAddressSize = subServiceSixConfig.getInt("addressSize", REGISTER_ADDRESS_SIZE);
+        registerValueSize = subServiceSixConfig.getInt("valueSize", REGISTER_VALUE_SIZE);
         registerStartOffset = 4;
 
         try{
-            registerDumpBucket = getBucket("deviceRegisterDump");
+            registerDumpBucket = getBucket("deviceRegisterDump", yamcsInstance);
         } catch (InitException e) {
             log.error("Unable to create a `deviceRegisterDump bucket` for (Service - 2 | SubService - 6)", e);
             throw new YarchException("Failed to create RDB based bucket: deviceRegisterDump", e);
@@ -51,22 +52,8 @@ public class SubServiceSix implements PusSubService {
         gson = new GsonBuilder().setPrettyPrinting().create();
     }
 
-    public Bucket getBucket(String bucketName) throws InitException {
-        YarchDatabaseInstance ydb = YarchDatabase.getInstance(yamcsInstance);
-
-        try {
-            Bucket bucket = ydb.getBucket(bucketName);
-            if (bucket == null) {
-                bucket = ydb.createBucket(bucketName);
-            }
-            return bucket;
-        } catch (IOException e) {
-            throw new InitException(e);
-        }
-    }
-
     @Override
-    public void process(PusTmPacket pusTmPacket) {
+    public TmPacket process(PusTmPacket pusTmPacket) {
         byte[] dataField = pusTmPacket.getDataField();
 
         int numberOfRegisters = ByteArrayUtils.decodeInt(dataField, 0);
@@ -93,5 +80,7 @@ public class SubServiceSix implements PusSubService {
         } catch(IOException e) {
             throw new UncheckedIOException("Cannot save device register dump report in bucket: " + registerDumpFileName + (registerDumpBucket != null ? " -> " + registerDumpBucket.getName() : ""), e);
         }
+
+        return pusTmPacket.getTmPacket();
     }
 }
