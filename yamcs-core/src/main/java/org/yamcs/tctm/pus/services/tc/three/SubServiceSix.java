3cae63165f2ddf5f185b9b9b76b8411e16abb7f6
fubaa <swarup@pixxel.co.in>
2023-10-09 14:14:39 +0530

    GSS-223: Add support for Service-3:SubService-6 | Disable Housekeeping Parameter Report Generation

---
+++ yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tc/three/SubServiceSix.java
@@ -0,0 +1,26 @@
+package org.yamcs.tctm.pus.services.tc.three;
+
+import org.yamcs.TmPacket;
+import org.yamcs.YConfiguration;
+import org.yamcs.commanding.PreparedCommand;
+import org.yamcs.tctm.pus.services.PusSubService;
+import org.yamcs.tctm.pus.services.tc.PusTcModifier;
+
+public class SubServiceSix implements PusSubService {
+    String yamcsInstance;
+
+    SubServiceSix(String yamcsInstance, YConfiguration config) {
+        this.yamcsInstance = yamcsInstance;
+    }
+
+    @Override
+    public PreparedCommand process(PreparedCommand telecommand) {
+        return PusTcModifier.setPusHeadersSpareFieldAndSourceID(telecommand);
+    }
+
+    @Override
+    public TmPacket process(TmPacket tmPacket) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'process'");
+    }    
+}
