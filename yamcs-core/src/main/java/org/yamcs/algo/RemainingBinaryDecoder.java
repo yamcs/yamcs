8bca43cacf3647ddb925de507f178d546118a141
Fabian Diet <fabian.diet@spaceapplications.com>
2023-12-15 22:52:58 +0100

    Add few more reusable data decoders

---
+++ yamcs-core/src/main/java/org/yamcs/algo/RemainingBinaryDecoder.java
@@ -0,0 +1,46 @@
+package org.yamcs.algo;
+
+import org.yamcs.algorithms.AlgorithmExecutionContext;
+import org.yamcs.mdb.AbstractDataDecoder;
+import org.yamcs.parameter.Value;
+import org.yamcs.utils.BitBuffer;
+import org.yamcs.utils.ValueUtility;
+import org.yamcs.xtce.CustomAlgorithm;
+import org.yamcs.xtce.DataEncoding;
+
+/**
+ * A decoder that returns a binary value containing all of the remaining bytes.
+ * <p>
+ * An example where this may be useful is a packet that contains an arbitrarily sized blob of data with no length
+ * indication.
+ * <p>
+ * The following XTCE snippet illustrates intended usage.
+ * 
+ * <pre>
+ * &lt;BinaryParameterType name="outputData"&gt;
+ *   &lt;BinaryDataEncoding&gt;
+ *     &lt;SizeInBits&gt;
+ *       &lt;DynamicValue&gt;
+ *         &lt;ParameterInstanceRef parameterRef="_yamcs_ignore"/&gt;
+ *       &lt;/DynamicValue&gt;
+ *     &lt;/SizeInBits&gt;
+ *     &lt;FromBinaryTransformAlgorithm name="org_yamcs_algo_RemainingBinaryDecoder"&gt;
+ *       &lt;AlgorithmText language="java"&gt;org.yamcs.algo.RemainingBinaryDecoder&lt;/AlgorithmText&gt;
+ *     &lt;/FromBinaryTransformAlgorithm&gt;
+ *   &lt;/BinaryDataEncoding&gt;
+ * &lt;/BinaryParameterType&gt;
+ * </pre>
+ */
+public class RemainingBinaryDecoder extends AbstractDataDecoder {
+
+    public RemainingBinaryDecoder(CustomAlgorithm alg, AlgorithmExecutionContext ctx) {
+        // Constructor required
+    }
+
+    @Override
+    public Value extractRaw(DataEncoding de, BitBuffer buffer) {
+        var b = new byte[buffer.remainingBytes()];
+        buffer.getByteArray(b);
+        return ValueUtility.getBinaryValue(b);
+    }
+}
---
+++ yamcs-core/src/main/java/org/yamcs/algo/ReverseBinaryDecoder.java
@@ -0,0 +1,54 @@
+package org.yamcs.algo;
+
+import org.yamcs.algorithms.AlgorithmExecutionContext;
+import org.yamcs.mdb.AbstractDataDecoder;
+import org.yamcs.parameter.Value;
+import org.yamcs.utils.BitBuffer;
+import org.yamcs.utils.ValueUtility;
+import org.yamcs.xtce.BinaryDataEncoding;
+import org.yamcs.xtce.CustomAlgorithm;
+import org.yamcs.xtce.DataEncoding;
+
+/**
+ * A custom data decoder that returns a binary value that has all the bytes reversed from the encoded binary.
+ * <p>
+ * This is intended to be used with a custom transformation for the {@link BinaryDataEncoding} of a binary parameter.
+ * <p>
+ * The implementation assumes a fixed-size encoding.
+ */
+public class ReverseBinaryDecoder extends AbstractDataDecoder {
+
+    public ReverseBinaryDecoder(CustomAlgorithm alg, AlgorithmExecutionContext ctx) {
+        // Constructor required
+    }
+
+    @Override
+    public Value extractRaw(DataEncoding de, BitBuffer buffer) {
+        var sizeInBits = de.getSizeInBits();
+        if (sizeInBits < 0) {
+            throw new IllegalArgumentException("Cannot decode binary parameter. Size is not fixed");
+        }
+
+        if (sizeInBits % 8 != 0) {
+            throw new IllegalArgumentException("Cannot decode binary parameter. Bit size should be multiple of 8");
+        }
+
+        var sizeInBytes = sizeInBits / 8;
+        if (sizeInBytes > buffer.remainingBytes()) {
+            throw new IndexOutOfBoundsException("Cannot decode binary parameter of size " + sizeInBytes
+                    + ". Remaining in the buffer: " + buffer.remainingBytes());
+        }
+
+        var bytes = new byte[sizeInBytes];
+        buffer.getByteArray(bytes);
+
+        // Reverse in-place
+        for (int i = 0; i < bytes.length / 2; i++) {
+            var temp = bytes[i];
+            bytes[i] = bytes[bytes.length - i - 1];
+            bytes[bytes.length - i - 1] = temp;
+        }
+
+        return ValueUtility.getBinaryValue(bytes);
+    }
+}
