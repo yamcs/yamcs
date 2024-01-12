8e98df1fc71f3be8b9f7ed4992e2c61b9bc97093
loricvdt <loric.vandentempel@spaceapplications.com>
2024-01-12 12:29:23 +0100

    Add CspPacket setHeader method

--- yamcs-core/src/main/java/org/yamcs/tctm/csp/CspPacket.java
+++ yamcs-core/src/main/java/org/yamcs/tctm/csp/CspPacket.java
@@ -71,6 +71,25 @@ public class CspPacket {
         packet[3] |= 0x01;
     }
 
+    public void setHeader(byte priority, byte source, byte destination, byte destinationPort, byte sourcePort) {
+        setHeader(priority, source, destination, destinationPort, sourcePort, false, false, false, false);
+    }
+
+    public void setHeader(byte priority, byte source, byte destination, byte destinationPort, byte sourcePort,
+            boolean hmacFlag, boolean xteaFlag, boolean rdpFlag, boolean crcFlag) {
+        int headerBits =
+                    (priority & 0b11) << 30 |
+                    (source & 0b11111) << 25 |
+                    (destination & 0b11111) << 20 |
+                    (destinationPort & 0b111111) << 14 |
+                    (sourcePort & 0b111111) << 8 |
+                    (byte) (hmacFlag ? (0b1 << 3) : 0b0) |
+                    (byte) (xteaFlag ? (0b1 << 2) : 0b0) |
+                    (byte) (rdpFlag ? (0b1 << 1) : 0b0) |
+                    (byte) (crcFlag ? 0b1 : 0b0);
+        bb.putInt(0, headerBits);
+    }
+
     public int getLength() {
         return bb.capacity();
     }
