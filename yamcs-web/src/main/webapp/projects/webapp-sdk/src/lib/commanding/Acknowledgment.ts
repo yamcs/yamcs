7f129f98c18fcdb5d181c908439b382560831f64
Fabian Diet <fabian.diet@spaceapplications.com>
2024-02-01 19:45:28 +0100

    Allow verifiers to return a value directly (<> message)
    Previously it was only possible to specify a
    "return parameter" on a verifier.
    
    With this commit, it is possible to return any kind of
    value. In cmdhist, there will be an attribute linked
    to a dummy parameter named /yamcs/cmd/returnValue.
    
    An example use case for this, is when a command is
    expected to return something that should be seen
    immediately, like is common with RPC-style communication
    flows.
    
    A returnValue can be specified both on success or fail.
    
    A returnValue can be specified in addition to a 'message'.

--- yamcs-web/src/main/webapp/projects/webapp-sdk/src/lib/commanding/Acknowledgment.ts
+++ yamcs-web/src/main/webapp/projects/webapp-sdk/src/lib/commanding/Acknowledgment.ts
@@ -1,6 +1,10 @@
+import { Value } from '../client/types/monitoring';
+
+
 export interface Acknowledgment {
   name?: string;
   status?: string;
   time?: string;
   message?: string;
+  returnValue?: Value;
 }
