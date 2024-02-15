060e431d0a1489f06076561ae6f05587d9d7639d
fubaa <swarup@pixxel.co.in>
2024-02-15 11:21:10 +0530

    GSS-285: Add implementation for Service-17 | Test

---
+++ yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tc/seventeen/SubServiceThree.java
@@ -0,0 +1,33 @@
+package org.yamcs.tctm.pus.services.tc.seventeen;
+
+import java.util.ArrayList;
+
+import org.yamcs.TmPacket;
+import org.yamcs.YConfiguration;
+import org.yamcs.commanding.PreparedCommand;
+import org.yamcs.tctm.pus.services.PusSubService;
+import org.yamcs.tctm.pus.services.tc.PusTcCcsdsPacket;
+
+public class SubServiceThree implements PusSubService {
+
+    String yamcsInstance;
+    YConfiguration config;
+
+    public SubServiceThree(String yamcsInstance, YConfiguration config) {
+        this.yamcsInstance = yamcsInstance;
+        this.config = config;
+    }
+
+
+    @Override
+    public PreparedCommand process(PreparedCommand telecommand) {
+        return PusTcCcsdsPacket.setPusHeadersSpareFieldAndSourceID(telecommand);
+    }
+
+    @Override
+    public ArrayList<TmPacket> process(TmPacket tmPacket) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'process'");
+    }
+    
+}
---
+++ yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tm/seventeen/SubServiceTwo.java
@@ -0,0 +1,33 @@
+package org.yamcs.tctm.pus.services.tm.seventeen;
+
+import java.util.ArrayList;
+
+import org.yamcs.TmPacket;
+import org.yamcs.YConfiguration;
+import org.yamcs.commanding.PreparedCommand;
+import org.yamcs.tctm.pus.services.PusSubService;
+
+public class SubServiceTwo implements PusSubService {
+
+    String yamcsInstance;
+    YConfiguration config;
+
+    public SubServiceTwo(String yamcsInstance, YConfiguration config) {
+        this.yamcsInstance = yamcsInstance;
+        this.config = config;
+    }
+
+    @Override
+    public PreparedCommand process(PreparedCommand telecommand) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'process'");
+    }
+
+    @Override
+    public ArrayList<TmPacket> process(TmPacket tmPacket) {
+        ArrayList<TmPacket> pPkts = new ArrayList<>();
+        pPkts.add(tmPacket);
+
+        return pPkts;
+    } 
+}
