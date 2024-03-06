046801c2d8e1ae49e6cba8e89c9bf63b4029a95c
Nicolae Mihalache <xpromache@gmail.com>
2024-03-06 13:11:34 +0100

    doc update

--- yamcs-core/src/main/java/org/yamcs/utils/PartitionedTimeInterval.java
+++ yamcs-core/src/main/java/org/yamcs/utils/PartitionedTimeInterval.java
@@ -6,8 +6,9 @@ import java.util.NoSuchElementException;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * A list of sorted, non overlapping {@link TimeInterval} The composing
- * time intervals are considered closed at start and open at end [start, end)
+ * A list of sorted, non overlapping {@link TimeInterval}
+ * <p>
+ * The intervals are considered closed at start and open at end [start, end)
  * <p>
  * It is implemented as a copy on write array and it is thread safe.
  * 
