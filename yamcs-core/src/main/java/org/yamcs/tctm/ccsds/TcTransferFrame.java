196645a443789e5930ff4983592bae1e8a916e7d
Nicolae Mihalache <xpromache@gmail.com>
2024-03-25 04:32:57 +0100

    typos, doc update and cleanup

--- yamcs-core/src/main/java/org/yamcs/tctm/ccsds/TcTransferFrame.java
+++ yamcs-core/src/main/java/org/yamcs/tctm/ccsds/TcTransferFrame.java
@@ -6,12 +6,29 @@ import org.yamcs.commanding.PreparedCommand;
 
 /**
  * 
- * @author nm
- *         TC Transfer Frame as per
- * 
- *         CCSDS RECOMMENDED STANDARD FOR TC SPACE DATA LINK PROTOCOL
- *         CCSDS 232.0-B-3 September 2015
+ * TC Transfer Frame as per
+ * <p>
+ * CCSDS RECOMMENDED STANDARD FOR TC SPACE DATA LINK PROTOCOL
+ * CCSDS 232.0-B-4 October 2021
  *
+ * <p>
+ * Frame structure:
+ * <ul>
+ * <li>Transfer Frame Primary Header (5 bytes mandatory)</li>
+ * <li>Transfer Frame Data Field (up to 1019 or 1017 bytes, mandatory)</li>
+ * <li>Frame Error Control Field (2 bytes, optional).</li>
+ * </ul>
+ * The Transfer Frame Primary Header structure:
+ * <ul>
+ * <li>Transfer Frame Version Number (2 bits, mandatory)</li>
+ * <li>Bypass Flag (1 bit, mandatory)</li>
+ * <li>Control Command Flag (1 bit, mandatory)</li>
+ * <li>Reserved Spare (2 bits, mandatory)</li>
+ * <li>Spacecraft Identifier (10 bits, mandatory)</li>
+ * <li>Virtual Channel Identifier (6 bits, mandatory)</li>
+ * <li>Frame Length (10 bits, mandatory)</li>
+ * <li>Frame Sequence Number (8 bits, mandatory)</li>
+ * </ul>
  */
 public class TcTransferFrame {
     private boolean cmdControl;
@@ -56,12 +73,36 @@ public class TcTransferFrame {
         return cmdControl ? 1 : 0;
     }
 
+    /**
+     * 
+     * returns the data of the frame - that is frame header + frame data + optional checksum
+     * <p>
+     * The frame data starts and ends at the offsets returned by {@link #getDataStart()} and {@link #getDataEnd()}
+     * respectively
+     */
     public byte[] getData() {
         return data;
     }
 
+    /**
+     * 
+     * returns the offset of the data start inside the frame
+     * <p>
+     * For CCSDS TC frames the offset is always 5
+     */
     public int getDataStart() {
         return dataStart;
+    }
+
+    /**
+     * 
+     * returns the offset of the data end inside the frame
+     * <p>
+     * For CCSDS TC frames this is the end of the frame or 2 bytes before depending whether error control is used or
+     * not.
+     */
+    public int getDataEnd() {
+        return dataEnd;
     }
 
     public void setVcFrameSeq(int vS) {
@@ -71,6 +112,7 @@ public class TcTransferFrame {
     public int getVcFrameSeq() {
         return vcFrameSeq;
     }
+
 
     public void setDataStart(int start) {
         this.dataStart = start;
