9f6bd780e2231458013e31783e1b85248a1202f6
Swarup Nataraj <swarup@pixxel.co.in>
2024-05-10 21:17:20 +0530

    Reimplementation: Fill the Last_Uplink_Part command with a File_Part_Size argument, with the actual size of the File_Part

--- yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tc/thirteen/SubServiceEleven.java
+++ yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tc/thirteen/SubServiceEleven.java
@@ -10,12 +10,22 @@ import org.yamcs.commanding.PreparedCommand;
 import org.yamcs.tctm.pus.PusTcManager;
 import org.yamcs.tctm.pus.services.PusSubService;
 import org.yamcs.tctm.pus.services.tc.PusTcCcsdsPacket;
+import org.yamcs.utils.ByteArrayUtils;
 
 public class SubServiceEleven implements PusSubService {
     String yamcsInstance;
 
+    protected final boolean DEFAULT_ADD_FILE_PART_SIZE = false;
+    protected final int DEFAULT_ADD_FILE_PART_SIZE_SIZE = 1;
+
+    protected boolean addFilePartSize;
+    protected int addFilePartSizeSize;
+
     SubServiceEleven(String yamcsInstance, YConfiguration config) {
         this.yamcsInstance = yamcsInstance;
+
+        addFilePartSize = config.getBoolean("addFilePartSize", DEFAULT_ADD_FILE_PART_SIZE);
+        addFilePartSizeSize = config.getInt("addFilePartSizeSize", DEFAULT_ADD_FILE_PART_SIZE_SIZE);
     }
 
     @Override
@@ -42,12 +52,20 @@ public class SubServiceEleven implements PusSubService {
         byte[] filePartActual = Arrays.copyOfRange(filePartSizeWithPadding, 0, filePartActualSize);
 
         // Obtain the new length of the packet
-        int newPacketLength = PusTcManager.DEFAULT_PRIMARY_HEADER_LENGTH + PusTcManager.secondaryHeaderLength + ServiceThirteen.largePacketTransactionIdSize + ServiceThirteen.partSequenceNumberSize + filePartActualSize;
+        int newPacketLength;
+        if (addFilePartSize)
+            newPacketLength = PusTcManager.DEFAULT_PRIMARY_HEADER_LENGTH + PusTcManager.secondaryHeaderLength + ServiceThirteen.largePacketTransactionIdSize + ServiceThirteen.partSequenceNumberSize + addFilePartSizeSize + filePartActualSize;
+        else
+            newPacketLength = PusTcManager.DEFAULT_PRIMARY_HEADER_LENGTH + PusTcManager.secondaryHeaderLength + ServiceThirteen.largePacketTransactionIdSize + ServiceThirteen.partSequenceNumberSize + filePartActualSize;
 
         // Create new packet
         ByteBuffer bb = ByteBuffer.wrap(new byte[newPacketLength]);
         bb.put(Arrays.copyOfRange(binary, 0, PusTcManager.DEFAULT_PRIMARY_HEADER_LENGTH + PusTcManager.secondaryHeaderLength + ServiceThirteen.largePacketTransactionIdSize
                 + ServiceThirteen.partSequenceNumberSize)); // Primary + Secondary + LargeId + PartSeqNumber
+
+        if (addFilePartSize)
+            bb.put(ByteArrayUtils.encodeCustomInteger(filePartActualSize, addFilePartSizeSize));
+
         bb.put(filePartActual);
 
         // Set Binary
