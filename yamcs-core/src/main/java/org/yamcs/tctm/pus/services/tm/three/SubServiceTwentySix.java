61785ea685360ac29e9ffb2def54d2f59daf1f86
Swarup Nataraj <swarup@pixxel.co.in>
2024-06-11 12:57:10 +0530

    Bugfix: Set the correct size for the diagnostic report ID
    Also remove unneeded github workflow files for now

--- yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tm/three/SubServiceTwentySix.java
+++ yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tm/three/SubServiceTwentySix.java
@@ -21,13 +21,15 @@ public class SubServiceTwentySix implements PusSubService {
     private int DEFAULT_SIMPLE_COMMUTATIVE_DIFFERENTIATOR = 1;
     private int DEFAULT_SUPER_COMMUTATIVE_DIFFERENTIATOR = 2;
     private int DEFAULT_COLLECTION_INTERVAL_SIZE = 4;
+    private int DEFAULT_HOUSEKEEPING_PARAMETER_REPORT_STRUCTURE_ID_SIZE = 4;
 
     private int simpleCommutatedSize;
     private int superCommutatedSampleRepetitionNumberSize;
     private int collectionIntervalSize;
+    private int diagnosticParameterReportStructureIDSize;
+
     private byte simpleCommutativeDifferentiator;
     private byte superCommutativeDifferentiator;
-
 
     SubServiceTwentySix(String yamcsInstance, YConfiguration config) {
         this.yamcsInstance = yamcsInstance;
@@ -36,6 +38,7 @@ public class SubServiceTwentySix implements PusSubService {
         simpleCommutatedSize = config.getInt("simpleCommutatedSize", DEFAULT_SIMPLE_COMMUTATED_SIZE);
         superCommutatedSampleRepetitionNumberSize = config.getInt("superCommutatedSampleRepetitionNumberSize", DEFAULT_SUPER_COMMUTATED_SAMPLE_REPETITION_NUMBER_SIZE);
         collectionIntervalSize = config.getInt("collectionIntervalSize", DEFAULT_COLLECTION_INTERVAL_SIZE);
+        diagnosticParameterReportStructureIDSize = config.getInt("diagnosticParameterReportStructureIDSize", DEFAULT_HOUSEKEEPING_PARAMETER_REPORT_STRUCTURE_ID_SIZE);
 
         simpleCommutativeDifferentiator = (byte) config.getInt("simpleCommutativeDifferentiator", DEFAULT_SIMPLE_COMMUTATIVE_DIFFERENTIATOR);
         superCommutativeDifferentiator = (byte) config.getInt("superCommutativeDifferentiator", DEFAULT_SUPER_COMMUTATIVE_DIFFERENTIATOR);
@@ -99,23 +102,22 @@ public class SubServiceTwentySix implements PusSubService {
         int superCommutatedSampleRepetitionNumber = (int) ByteArrayUtils.decodeCustomInteger(spareField, simpleCommutatedSize, superCommutatedSampleRepetitionNumberSize);
         int collectionInterval = (int) ByteArrayUtils.decodeCustomInteger(spareField, (simpleCommutatedSize + superCommutatedSampleRepetitionNumberSize), collectionIntervalSize);
 
-        int housekeepingParameterReportStructureIDSize = 4;
         byte[] housekeepingParameterReportStructureID = Arrays.copyOfRange(
             dataField,
             0,
-            housekeepingParameterReportStructureIDSize
+            diagnosticParameterReportStructureIDSize
         );
         byte[] simpleCommutatedParameterArray = Arrays.copyOfRange(
             dataField,
-            housekeepingParameterReportStructureIDSize,
-            (housekeepingParameterReportStructureIDSize + simpleCommutatedLength)
+            diagnosticParameterReportStructureIDSize,
+            (diagnosticParameterReportStructureIDSize + simpleCommutatedLength)
         );
         pPkts.add(
             createSimpleCommutativePusTmPacket(tmPacket, housekeepingParameterReportStructureID, simpleCommutatedParameterArray)
         );
 
         if (superCommutatedSampleRepetitionNumber != 0) {
-            byte[] superCommutedParameterArray = Arrays.copyOfRange(dataField, (housekeepingParameterReportStructureIDSize + simpleCommutatedLength), dataField.length);
+            byte[] superCommutedParameterArray = Arrays.copyOfRange(dataField, (diagnosticParameterReportStructureIDSize + simpleCommutatedLength), dataField.length);
             int superCommutatedParameterSubStructureSize = (int) superCommutedParameterArray.length / superCommutatedSampleRepetitionNumber; // FIXME: No need to typecast, since it will always be perfectly divisbible
 
             long gentime = tmPacket.getGenerationTime();
@@ -138,5 +140,4 @@ public class SubServiceTwentySix implements PusSubService {
 
         return pPkts;
     }
- 
 }
