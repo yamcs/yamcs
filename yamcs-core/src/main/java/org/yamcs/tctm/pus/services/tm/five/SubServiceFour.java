76848940576c49130d5a9c1fd3b6fb9c3aeb3944
Swarup Nataraj <swarup@pixxel.co.in>
2024-07-11 19:18:47 +0530

    Update S5 implementations for all Sub-1,2,3,4 subservices

--- yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tm/five/SubServiceFour.java
+++ yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tm/five/SubServiceFour.java
@@ -1,6 +1,7 @@
 package org.yamcs.tctm.pus.services.tm.five;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.yamcs.TmPacket;
 import org.yamcs.YConfiguration;
@@ -9,32 +10,81 @@ import org.yamcs.events.EventProducer;
 import org.yamcs.events.EventProducerFactory;
 import org.yamcs.logging.Log;
 import org.yamcs.tctm.pus.services.PusSubService;
+import org.yamcs.tctm.pus.services.tm.PusTmCcsdsPacket;
+import org.yamcs.tctm.pus.services.tm.one.ServiceOne;
+import org.yamcs.tctm.pus.tuples.Pair;
+import org.yamcs.tctm.pus.tuples.Triple;
+import org.yamcs.utils.ByteArrayUtils;
 
 public class SubServiceFour implements PusSubService {
     String yamcsInstance;
     Log log;
 
     EventProducer eventProducer;
-    static final String source = "Service: 5 | SubService: 4";
+    static String source = "Service: 5 | SubService: 4";
 
     public SubServiceFour(String yamcsInstance, YConfiguration subServiceSixConfig) {
         this.yamcsInstance = yamcsInstance;
         log = new Log(getClass(), yamcsInstance);
 
-        // FIXME: Confirm the repeatedEventTimeoutMillisec value, which most likely depends on the datarate of TM
         eventProducer = EventProducerFactory.getEventProducer(yamcsInstance, this.getClass().getSimpleName(), 10);
         eventProducer.setSource(source);
     }
 
     @Override
     public PreparedCommand process(PreparedCommand pusTelecommand) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'process'");
     }
 
     @Override
     public ArrayList<TmPacket> process(TmPacket tmPacket) {
-        // FIXME: ToDo | Send event notification as per the deduced auxillary data
+        PusTmCcsdsPacket pPkt = new PusTmCcsdsPacket(tmPacket.getPacket());
+        byte[] dataField = pPkt.getDataField();
+
+        int apid = pPkt.getAPID();
+        int eventId = (int) ByteArrayUtils.decodeCustomInteger(dataField, 0, ServiceFive.eventIdSize);
+
+        Pair<String, Map<Integer, Triple<Integer, String, Map<Integer, Pair<Integer, String>>>>> eventMap = ServiceFive.eventIds.get(new Pair<>(apid, eventId));
+        Map<Integer, Triple<Integer, String, Map<Integer, Pair<Integer, String>>>> chunkMap = eventMap.getSecond();
+
+        String eventDec = "EventName: " + eventMap.getFirst() + " | EventId: " + eventId;
+
+        if (chunkMap != null) {
+            for (Map.Entry<Integer, Triple<Integer, String, Map<Integer, Pair<Integer, String>>>> chunk: chunkMap.entrySet()) {
+                Triple<Integer, String, Map<Integer, Pair<Integer, String>>> chunkDeets = chunk.getValue();
+
+                int chunkOffset = chunk.getKey();
+                int chunkLength = chunkDeets.getFirst();
+                String chunkName = chunkDeets.getSecond();
+                Map<Integer, Pair<Integer, String>> bitsMap = chunkDeets.getThird();
+
+                if (bitsMap != null) {
+                    long data = ByteArrayUtils.decodeCustomInteger(dataField, chunkOffset + ServiceFive.eventIdSize, chunkLength);
+
+                    for (Map.Entry<Integer, Pair<Integer, String>> bits: bitsMap.entrySet()) {
+                        long mask = ServiceFive.createOnes(chunkLength);
+                        Pair<Integer, String> bitsDeets = bits.getValue();
+
+                        int bitLength = bitsDeets.getFirst();
+                        int bitOffset = bits.getKey() + bitLength;
+                        String bitName = bitsDeets.getSecond();
+
+                        // Generate bitMask
+                        mask = ((~(mask >> bitOffset)) << (bitOffset - bitLength)) >> (bitOffset - bitLength);
+
+                        long disValue = data & mask;
+                        eventDec += " | " + bitName + ": " + disValue;
+                    }
+                    continue;
+                }
+
+                long data = ByteArrayUtils.decodeCustomInteger(dataField, chunkOffset + ServiceFive.eventIdSize, chunkLength);
+                eventDec += " | " + chunkName + ": " + data;
+            }
+        }
+
+        eventDec += " is thrown";
+        eventProducer.sendCritical(ServiceOne.CcsdsApid.fromValue(apid).name(), eventDec);
 
         ArrayList<TmPacket> pPkts = new ArrayList<>();
         pPkts.add(tmPacket);
