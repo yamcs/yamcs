45a64b21719c65679fe3a63aea38e7da3b2af9aa
Fabian Diet <fabian.diet@spaceapplications.com>
2024-08-07 14:55:55 +0200

    Combine file transfer in single list, add filters

--- yamcs-core/src/main/java/org/yamcs/filetransfer/FileListingService.java
+++ yamcs-core/src/main/java/org/yamcs/filetransfer/FileListingService.java
@@ -1,13 +1,16 @@
 package org.yamcs.filetransfer;
 
+import java.util.Map;
+import java.util.Set;
+
 import org.yamcs.InitException;
 import org.yamcs.YConfiguration;
 import org.yamcs.protobuf.ListFilesResponse;
 
-import java.util.Map;
-import java.util.Set;
-
 public interface FileListingService {
+
+    default void init(String yamcsInstance, String serviceName, YConfiguration config) throws InitException {
+    }
 
     void registerRemoteFileListMonitor(RemoteFileListMonitor monitor);
 
@@ -17,36 +20,32 @@ public interface FileListingService {
 
     Set<RemoteFileListMonitor> getRemoteFileListMonitors();
 
-    default void init(String yamcsInstance, String serviceName, YConfiguration config) throws InitException {
-    }
-
     /**
      * Return latest file list of the given destination.
      *
      * @param source
-     *         source requesting the file list (e.g. local entity for CFDP)
+     *            source requesting the file list (e.g. local entity for CFDP)
      * @param destination
-     *         destination from which the file list is needed (e.g. remote entity for CFDP)
+     *            destination from which the file list is needed (e.g. remote entity for CFDP)
      * @param remotePath
-     *         path on the destination from which to get the file list
+     *            path on the destination from which to get the file list
      * @param options
-     *         reliability of the file listing request (e.g. transmission mode for CFDP, may not be needed)
+     *            reliability of the file listing request (e.g. transmission mode for CFDP, may not be needed)
      * @return file list
      */
-    // TODO: maybe move "reliable" into a map
     ListFilesResponse getFileList(String source, String destination, String remotePath, Map<String, Object> options);
 
     /**
      * Start fetching a new file list from remote.
      *
      * @param source
-     *         source requesting the file list (e.g. local entity for CFDP)
+     *            source requesting the file list (e.g. local entity for CFDP)
      * @param destination
-     *         destination from which the file list is needed (e.g. remote entity for CFDP)
+     *            destination from which the file list is needed (e.g. remote entity for CFDP)
      * @param remotePath
-     *         path on the destination from which to get the file list
+     *            path on the destination from which to get the file list
      * @param options
-     *         reliability of the file listing request (e.g. transmission mode for CFDP, may not be needed)
+     *            reliability of the file listing request (e.g. transmission mode for CFDP, may not be needed)
      */
     void fetchFileList(String source, String destination, String remotePath, Map<String, Object> options);
 
