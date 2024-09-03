7122fa63acc1daa39680b7abd99fd80124fce2f5
Nicolae Mihalache <xpromache@gmail.com>
2024-09-03 15:35:48 +0200

    Added a binary time decoder for parameters
    This allows easier (than in pure XTCE) decoding of parameter times and using leap seconds as well as time correlation service.

--- yamcs-core/src/main/java/org/yamcs/mdb/DataEncoderFactory.java
+++ yamcs-core/src/main/java/org/yamcs/mdb/DataEncoderFactory.java
@@ -1,20 +1,23 @@
 package org.yamcs.mdb;
 
+import org.yamcs.algorithms.AlgorithmExecutionContext;
 import org.yamcs.xtce.Algorithm;
 import org.yamcs.xtce.CustomAlgorithm;
 
 public class DataEncoderFactory {
-    public static DataEncoder get(Algorithm a) {
-        if(!(a instanceof CustomAlgorithm)) {
-            throw new XtceProcessingException("Unsupported algorithm: '"+a+"'. Only Java custom algorithms supported");
+    public static DataEncoder get(Algorithm a, ProcessorData pdata) {
+        if (!(a instanceof CustomAlgorithm)) {
+            throw new XtceProcessingException(
+                    "Unsupported algorithm: '" + a + "'. Only Java custom algorithms supported");
         }
-        CustomAlgorithm ca = (CustomAlgorithm)a;
+        CustomAlgorithm ca = (CustomAlgorithm) a;
 
-        if(!"java".equals(ca.getLanguage().toLowerCase())) {
-            throw new XtceProcessingException("Unsupported language for Data Encoder: '"+ca.getLanguage()+"'. Only Java supported");
+        if (!"java".equals(ca.getLanguage().toLowerCase())) {
+            throw new XtceProcessingException(
+                    "Unsupported language for Data Encoder: '" + ca.getLanguage() + "'. Only Java supported");
         }
-
-        return DataDecoderFactory.loadJavaAlgo(ca, null);
+        AlgorithmExecutionContext execCtx = new AlgorithmExecutionContext("DataEncoder", pdata, Integer.MAX_VALUE);
+        return DataDecoderFactory.loadJavaAlgo(ca, execCtx);
     }
 
 }
