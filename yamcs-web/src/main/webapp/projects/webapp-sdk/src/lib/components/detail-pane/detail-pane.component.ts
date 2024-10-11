56fa4d8867b1a56276dcf56827b9a943a0e0b473
Jose Manuel Diez <j.diezlopez@tu-berlin.de>
2024-10-11 14:48:06 +0200

    webapp: make detail pane collapsible

--- yamcs-web/src/main/webapp/projects/webapp-sdk/src/lib/components/detail-pane/detail-pane.component.ts
+++ yamcs-web/src/main/webapp/projects/webapp-sdk/src/lib/components/detail-pane/detail-pane.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   standalone: true,
@@ -6,6 +9,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './detail-pane.component.html',
   styleUrl: './detail-pane.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule],
 })
 export class YaDetailPane {
+  @Input() collapsed = false;
+
+  toggleCollapsed() {
+    this.collapsed = !this.collapsed;
+  }
 }
