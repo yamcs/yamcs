d25e5415582240a446a78033ead399f039e83745
Fabian Diet <fabian.diet@spaceapplications.com>
2025-01-14 15:27:31 +0100

    Avoid NPE on pcontext

--- yamcs-core/src/main/java/org/yamcs/mdb/ContainerDecodingException.java
+++ yamcs-core/src/main/java/org/yamcs/mdb/ContainerDecodingException.java
@@ -21,23 +21,25 @@ public class ContainerDecodingException extends XtceProcessingException {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Error processing container: ");
-        SequenceEntry se = pcontext.currentEntry;
-        if (se != null) {
-            sb.append("while extracting ");
-            if (se instanceof ContainerEntry) {
-                ContainerEntry ce = (ContainerEntry) se;
-                sb.append("container " + ce.getRefContainer().getQualifiedName());
-            } else if (se instanceof ParameterEntry) {
-                ParameterEntry pe = (ParameterEntry) se;
-                sb.append("parameter " + pe.getParameter().getQualifiedName());
-            } else if (se instanceof IndirectParameterRefEntry) {
-                IndirectParameterRefEntry ipre = (IndirectParameterRefEntry) se;
-                sb.append("indirect parameter " + ipre.getParameterRef());
+        if (pcontext != null) {
+            SequenceEntry se = pcontext.currentEntry;
+            if (se != null) {
+                sb.append("while extracting ");
+                if (se instanceof ContainerEntry) {
+                    ContainerEntry ce = (ContainerEntry) se;
+                    sb.append("container " + ce.getRefContainer().getQualifiedName());
+                } else if (se instanceof ParameterEntry) {
+                    ParameterEntry pe = (ParameterEntry) se;
+                    sb.append("parameter " + pe.getParameter().getQualifiedName());
+                } else if (se instanceof IndirectParameterRefEntry) {
+                    IndirectParameterRefEntry ipre = (IndirectParameterRefEntry) se;
+                    sb.append("indirect parameter " + ipre.getParameterRef());
+                }
             }
+            sb.append(" at bit position ")
+                    .append(pcontext.buffer.getPosition())
+                    .append(": ");
         }
-        sb.append(" at bit position ")
-                .append(pcontext.buffer.getPosition())
-                .append(": ");
 
         sb.append(getMessage());
         return sb.toString();
