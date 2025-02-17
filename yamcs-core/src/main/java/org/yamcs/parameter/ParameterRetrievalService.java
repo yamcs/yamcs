0a4ac8758bd4df6425f4e1de912767d2b9f7857b
Nicolae Mihalache <xpromache@gmail.com>
2025-02-17 19:30:47 +0100

    wip

--- yamcs-core/src/main/java/org/yamcs/parameter/ParameterRetrievalService.java
+++ yamcs-core/src/main/java/org/yamcs/parameter/ParameterRetrievalService.java
@@ -650,7 +650,6 @@ public class ParameterRetrievalService extends AbstractYamcsService {
         };
         processor.getParameterRequestManager().addRequest(params.keySet(), prmConsumer);
         processor.startAsync();
-        processor.startAsync();
         processor.awaitTerminated();
 
         return tc;
