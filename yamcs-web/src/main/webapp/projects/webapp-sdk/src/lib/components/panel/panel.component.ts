4d3111a12790580691a3e5b904e21758ddf5589c
Fabian Diet <fabian.diet@spaceapplications.com>
2025-03-15 20:13:00 +0100

    Add <ya-panel /> component

---
+++ yamcs-web/src/main/webapp/projects/webapp-sdk/src/lib/components/panel/panel.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ya-panel',
+  template: '<ng-content />',
+  styleUrl: './panel.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    'class': 'ya-panel',
+  },
+})
+export class YaPanel {
+}
