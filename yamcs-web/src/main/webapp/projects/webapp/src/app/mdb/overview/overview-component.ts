4eb2bc75c8320303bcbd2aa8090f67548d7d1f51
Swarup Nataraj <swarup@pixxel.co.in>
2026-02-25 16:18:17 +0530

    feat: add a table view component to mdb overview page to display the version and the history of each space system (but not the subsystem, since it is assumed that oftentimes the history is tracked at the space system level, and not the subsystem level)

--- yamcs-web/src/main/webapp/projects/webapp/src/app/mdb/overview/overview-component.ts
+++ yamcs-web/src/main/webapp/projects/webapp/src/app/mdb/overview/overview-component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+
 import {
   MissionDatabase,
   WebappSdkModule,
@@ -7,7 +8,9 @@ import {
 } from '@yamcs/webapp-sdk';
 
 @Component({
+  selector: 'app-mdb-overview',
   templateUrl: './overview-component.html',
+  styleUrls: ['./overview-component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [WebappSdkModule],
 })
@@ -19,6 +22,43 @@ export class OverviewComponent {
     title: Title,
   ) {
     title.setTitle('Mission database');
-    this.mdb$ = yamcs.yamcsClient.getMissionDatabase(yamcs.instance!);
+    this.mdb$ = this.yamcs.yamcsClient.getMissionDatabase(
+      this.yamcs.instance!,
+    );
+  }
+
+  /* -------------------------------
+   TrackBy helpers
+  --------------------------------*/
+
+  trackByName(_: number, item: any): string {
+    return item.name;
+  }
+
+  trackByVersion(_: number, item: any): string {
+    return item.version;
+  }
+
+  /* -------------------------------
+   Message helpers
+  --------------------------------*/
+
+  hasUrl(message: string): boolean {
+    return message.includes('http');
+  }
+
+  extractUrl(message: string): string | null {
+    const match = message.match(/https?:\/\/\S+/);
+    return match?.[0] ?? null;
+  }
+
+  extractText(message: string): string {
+    return message.replace(/https?:\/\/\S+/, '').trim();
+  }
+
+  formatMessage(message: string): string {
+    return message
+      .replace(/\n/g, '<br>')
+      .replace(/ {2,}/g, (spaces) => '&nbsp;'.repeat(spaces.length));
   }
 }
